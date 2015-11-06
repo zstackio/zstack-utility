149ac43688da8f3f6d7ad253b33fb78a24dfda6f
Frank Zhang <xing5820@gmail.com>
2015-11-05 23:06:50 -0800

    fix flat DHCP IP not configured if there is already an IP on the interface

--- kvmagent/kvmagent/plugins/mevoco.py
+++ kvmagent/kvmagent/plugins/mevoco.py
@@ -87,7 +87,7 @@ if [ $? -ne 0 ]; then
     exit_on_error
 fi
 
-ip netns exec $BR_NAME ip addr show $INNER_DEV | grep inet > /dev/null
+ip netns exec $BR_NAME ip addr show $INNER_DEV | grep $DHCP_IP > /dev/null
 if [ $? -ne 0 ]; then
     ip netns exec $BR_NAME ip addr add $DHCP_IP/$DHCP_NETMASK dev $INNER_DEV
     ip netns exec $BR_NAME ip link set $INNER_DEV up
