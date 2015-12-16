3881957a11d9a658ffd6f659a4ac50bc697353a2
youyk <yongkang.you@gmail.com>
2015-12-16 11:11:36 +0800

    refine create_yum.repo.sh

--- zstackbuild/scripts/create_yum_repo.sh
+++ zstackbuild/scripts/create_yum_repo.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#Danguous script, which will remove rpms. Please run it in a new minimal isolated system. 
+for item in `cat pkg_list`;do rpm -ev $item --nodeps; done
 # You need to install a very clean and basic host to download the yum repo.
 #rhel6
 #yum install -y unzip rabbitmq-server mysql-server mysql java-1.7.0-openjdk libselinux-python python-devel python-setuptools python-pip gcc autoconf iputils tcpdump ethtool haproxy dnsmasq wget curl qemu-img scsi-target-utils httpd openssh-clients openssh-server sshpass sudo ntp ntpdate bzip2 mysql qemu-kvm bridge-utils libvirt-python libvirt nfs-utils vconfig libvirt-client iptables tar gzip libaio lvm2 lvm2-libs device-mapper-multipath-libs device-mapper-multipath net-tools rsync dnsmasq lighttpd iproute vconfig daemonize --downloadonly --downloaddir=repo 
