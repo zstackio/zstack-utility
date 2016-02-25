b2f77086c16eb5e094f7aae6c38ce34fe0d1bb4e
Frank Zhang <xing5820@gmail.com>
2016-02-25 21:06:48 +0800

    fix2 : support cpu socket for windows

--- kvmagent/kvmagent/plugins/vm_plugin.py
+++ kvmagent/kvmagent/plugins/vm_plugin.py
@@ -1531,7 +1531,7 @@ class Vm(object):
             else:
                 cpu = e(root, 'cpu')
 
-            e(cpu, 'topology', attrib={'sockets': str(cmd.socketNum), 'cores': str(cmd.cpuOnSocket)})
+            e(cpu, 'topology', attrib={'sockets': str(cmd.socketNum), 'cores': str(cmd.cpuOnSocket), 'threads': '1'})
 
         def make_memory():
             root = elements['root']
