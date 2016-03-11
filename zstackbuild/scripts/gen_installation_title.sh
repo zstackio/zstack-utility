253bf14d44eb6f2feb4d88d24992c28ad36a9421
youyk <yongkang.you@gmail.com>
2016-03-12 00:31:35 +0800

    remove 'v' in product version string.

--- zstackbuild/scripts/gen_installation_title.sh
+++ zstackbuild/scripts/gen_installation_title.sh
@@ -4,5 +4,5 @@ output_file=$3
 
 which figlet
 if [ $? -eq 0 ]; then
-    figlet -w 80 $product v$version INSTALLATION |tee -a $output_file
+    figlet -w 80 $product $version INSTALLATION |tee -a $output_file
 fi
