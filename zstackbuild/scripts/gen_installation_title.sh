d6ee8aa2cde540735c67fe2fcd7d75dee7065d45
youyk <yongkang.you@gmail.com>
2016-03-12 13:05:24 +0800

    update figlet output and comment out product.version in build.properties

--- zstackbuild/scripts/gen_installation_title.sh
+++ zstackbuild/scripts/gen_installation_title.sh
@@ -1,8 +1,10 @@
-product=$1
+product=${1^^}
 version=$2
 output_file=$3
 
 which figlet
 if [ $? -eq 0 ]; then
-    figlet -w 80 $product $version INSTALLATION |tee -a $output_file
+    figlet -w 80 $product |tee -a $output_file
+    figlet -w 80 v $version |tee -a $output_file
+    figlet -w 80 INSTALLATION |tee -a $output_file
 fi
