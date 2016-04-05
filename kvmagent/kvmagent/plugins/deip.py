5ddc3e613dd6e9b97929b1d18bdc202dd6ab1a01
Frank Zhang <xing5820@gmail.com>
2016-04-05 15:58:23 +0800

    working on deip

--- kvmagent/kvmagent/plugins/deip.py
+++ kvmagent/kvmagent/plugins/deip.py
@@ -16,6 +16,11 @@ from jinja2 import Template
 
 logger = log.get_logger(__name__)
 
+class AgentRsp(object):
+    def __init__(self):
+        self.success = True
+        self.error = None
+
 class DEip(kvmagent.KvmAgent):
 
     APPLY_EIP_PATH = "/flatnetworkprovider/eip/apply"
@@ -24,103 +29,277 @@ class DEip(kvmagent.KvmAgent):
     BATCH_DELETE_EIP_PATH = "/flatnetworkprovider/eip/batchdelete"
 
     def start(self):
-        pass
+        http_server = kvmagent.get_http_server()
+
+        http_server.register_async_uri(self.APPLY_EIP_PATH, self.apply_eip)
+        http_server.register_async_uri(self.BATCH_APPLY_EIP_PATH, self._apply_eips)
+        http_server.register_async_uri(self.DELETE_EIP_PATH, self.delete_eip)
+        http_server.register_async_uri(self.BATCH_DELETE_EIP_PATH, self.delete_eips)
 
     def stop(self):
         pass
 
-    def _apply_eips(self, eips):
-        create_eip_cmd = '''
+    @kvmagent.replyerror
+    def apply_eip(self, req):
+        cmd = jsonobject.loads(req[http.REQUEST_BODY])
+        self._apply_eips([cmd.eip])
+        return jsonobject.dumps(AgentRsp())
+
+    @kvmagent.replyerror
+    def apply_eips(self, req):
+        cmd = jsonobject.loads(req[http.REQUEST_BODY])
+        self._apply_eips(cmd.eips)
+        return jsonobject.dumps(AgentRsp())
+
+    @kvmagent.replyerror
+    def delete_eips(self, req):
+        cmd = jsonobject.loads(req[http.REQUEST_BODY])
+        self._delete_eips(cmd.eips)
+        return jsonobject.dumps(AgentRsp())
+
+    @kvmagent.replyerror
+    def delete_eip(self, req):
+        cmd = jsonobject.loads(req[http.REQUEST_BODY])
+        self._delete_eips([cmd.eip])
+        return jsonobject.dumps(AgentRsp())
+
+    @lock.lock('eip')
+    def _delete_eips(self, eips):
+        delete_eip_cmd = '''
 PUB_BR={{publicBridgeName}}
-OUTER_DEV={{odev}}
-INNER_DEV={{idev}}
-BR_NAME="{{vmBridgeName}}"
+PUB_ODEV={{pub_odev}}
+PUB_IDEV={{pub_idev}}
+PRI_ODEV={{pri_odev}}
+PRI_IDEV={{pri_idev}}
+PRI_BR="{{vmBridgeName}}"
 VIP="{{vip}}"
 VIP_NETMASK="{{vipNetmask}}"
 VIP_GW="{{vipGateway}}"
-VM_NIC="{{nicName}}"
+NIC_NAME="{{nicName}}"
+NIC_GATEWAY="{{nicGateway}}"
+NIC_NETMASK="{{nicNetmask}}"
+NS_NAME="$PRI_BR"_"$PUB_BR"
+EBTABLE_CHAIN_NAME={{ebtable_chain_name}}
+
+NS="ip netns exec $NS_NAME"
 
 exit_on_error() {
     [ $? -ne 0 ] && exit 1
 }
 
-exit_on_error_with_msg() {
-    [ $? -ne 0 ] && exit 1
-    exit 1
+delete_chain_if_needed() {
+    eval $NS iptables-save | grep ":$1" > /dev/null
+    if [ $? -eq 0 ]; then
+        eval $NS iptables $1 -F $2
+        exit_on_error
+        eval $NS iptables $1 -X $2
+        exit_on_error
+    fi
 }
 
-ip netns exec $BR_NAME ip link show
-exit_on_error_with_msg "cannot find the namespace $BR_NAME"
+delete_eip_rules() {
+    DNAT_NAME="DNAT-$VIP"
+    FWD_NAME="FWD-$VIP"
+    SNAT_NAME="SNAT-$VIP"
+
+    delete_chain_if_needed "-t nat" $DNAT_NAME
+    delete_chain_if_needed "-t nat" $SNAT_NAME
+    delete_chain_if_needed "-t nat" $FWD_NAME
+}
+
+ip link del $PUB_ODEV &> /dev/null
+exit_on_error
+
+delete_eip_rules
+
+exit 0
+'''
+        for eip in eips:
+            ctx = {
+                "pub_odev": "%s_o" % eip.vip.replace(".", ""),
+                "pub_idev": "%s_i" % eip.vip.replace(".", ""),
+                "pri_odev": "$s_o" % eip.nicGateway.replace(".", ""),
+                "pri_idev": "$s_i" % eip.nicGateway.replace(".", ""),
+                "ebtable_chain_name": eip.nicGateway,
+            }
+            ctx.update(eip.__dict__)
+            tmpt = Template(delete_eip_cmd)
+            cmd = tmpt.render(ctx)
+            shell.call(cmd)
+
+    @lock.lock('eip')
+    def _apply_eips(self, eips):
+        create_eip_cmd = '''
+PUB_BR={{publicBridgeName}}
+PUB_ODEV={{pub_odev}}
+PUB_IDEV={{pub_idev}}
+PRI_ODEV={{pri_odev}}
+PRI_IDEV={{pri_idev}}
+PRI_BR="{{vmBridgeName}}"
+VIP="{{vip}}"
+VIP_NETMASK="{{vipNetmask}}"
+VIP_GW="{{vipGateway}}"
+NIC_NAME="{{nicName}}"
+NIC_GATEWAY="{{nicGateway}}"
+NIC_NETMASK="{{nicNetmask}}"
+NS_NAME="$PRI_BR"_"$PUB_BR"
+EBTABLE_CHAIN_NAME={{ebtable_chain_name}}
+
+NS="ip netns exec $NS_NAME"
+
+exit_on_error() {
+    [ $? -ne 0 ] && exit 1
+}
 
 # in case the namespace deleted and the orphan outer link leaves in the system,
 # deleting the orphan link and recreate it
-ip netns exec $$BR_NAME ip link | grep $INNER_DEV > /dev/null
-if [ $? -ne 0 ]; then
-   ip link del $OUTER_DEV &> /dev/null
-fi
+delete_orphan_outer_dev() {
+    ip netns exec $1 ip link | grep $2 > /dev/null
+    if [ $? -ne 0 ]; then
+        ip link del $3 &> /dev/null
+        exit_on_error
+    fi
+}
 
-ip link | grep $OUTER_DEV > /dev/null
+create_dev_if_needed() {
+    ip link | grep $1 > /dev/null
+    if [ $? -ne 0 ]; then
+        ip link add $1 type veth peer name $2
+        exit_on_error
+    fi
+
+    ip link set $1 up
+    exit_on_error
+}
+
+add_dev_to_br_if_needed() {
+    brctl show $1 | grep $2 > /dev/null
+    if [ $? -ne 0 ]; then
+        brctl addif $1 $2
+        exit_on_error
+    fi
+}
+
+add_dev_to_namespace_if_needed() {
+    eval $NS ip link | grep $1 > /dev/null
+    if [ $? -ne 0 ]; then
+        ip link set $1 netns $2
+        exit_on_error
+    fi
+}
+
+set_ip_to_idev_if_needed() {
+    eval $NS ip addr show $1 | grep $2 > /dev/null
+    if [ $? -ne 0 ]; then
+        eval $NS ip addr flush dev $1
+        exit_on_error
+        eval $NS ip addr add $2/$3 dev $1
+        exit_on_error
+    fi
+
+    eval $NS ip link set $1 up
+    exit_on_error
+}
+
+block_arp_for_nic_gw() {
+    local BR_PHY_DEV=`brctl show $PRI_BR | grep $PRI_BR | sed 's/\s\s*/ /g' | cut -d' ' -f4`
+    if [ x$BR_PHY_DEV == "x" ]; then
+       echo "cannot find the physical interface of the bridge $PRI_BR"
+       exit 1
+    fi
+
+    ebtables-save | grep ":$EBTABLE_CHAIN_NAME" > /dev/null
+    if [ $? -ne 0 ]; then
+        ebtables -N $EBTABLE_CHAIN_NAME
+        ebtables -I FORWARD -j $EBTABLE_CHAIN_NAME
+    fi
+
+    ebtables-save | grep "$EBTABLE_CHAIN_NAME -p ARP -o $BR_PHY_DEV --arp-ip-dst $NIC_GW -j DROP" > /dev/null
+    if [ $? -ne 0 ]; then
+        ebtables -I $EBTABLE_CHAIN_NAME -p ARP -o $BR_PHY_DEV --arp-ip-dst $NIC_GW -j DROP
+        exit_on_error
+    fi
+
+    ebtables-save | grep "$EBTABLE_CHAIN_NAME -p ARP -i $BR_PHY_DEV --arp-ip-dst $NIC_GW -j DROP" > /dev/null
+    if [ $? -ne 0 ]; then
+        ebtables -I $EBTABLE_CHAIN_NAME -p ARP -i $BR_PHY_DEV --arp-ip-dst $NIC_GW -j DROP
+        exit_on_error
+    fi
+}
+
+create_ip_table_rule_if_needed() {
+   eval $NS iptables-save | grep "$1" > /dev/null
+   if [ $? -ne 0 ]; then
+       eval $NS iptables $1 $2
+       exit_on_error
+   fi
+}
+
+set_eip_rules() {
+    DNAT_NAME=":DNAT-$VIP"
+    eval $NS iptables-save | grep "$DNAT_NAME" > /dev/null
+    if [ $? -ne 0 ]; then
+        eval $NS iptables -N $DNAT_NAME
+    fi
+
+    create_ip_table_rule_if_needed "-t nat" "-A PREROUTING -d $VIP -j $DNAT_NAME"
+    create_ip_table_rule_if_needed "-t nat" "-A $DNAT_NAME -j --to-destination $NIC_GW"
+
+    FWD_NAME=":FWD-$VIP"
+    eval $NS iptables-save | grep "$FWD_NAME" > /dev/null
+    if [ $? -ne 0 ]; then
+        eval $NS iptables -N $FWD_NAME
+    fi
+
+    create_ip_table_rule_if_needed "-t filter" "-A FORWARD -i $PRI_IDEV -o $PUB_IDEV -j $FWD_NAME"
+    create_ip_table_rule_if_needed "-t filter" "-A FORWARD -i $PUB_IDEV -o $PRI_IDEV -j $FWD_NAME"
+    create_ip_table_rule_if_needed "-t filter" "-A $FWD_NAME -j ACCEPT"
+
+    SNAT_NAME=":SNAT-$VIP"
+    if [ $? -ne 0 ]; then
+        eval $NS iptables -N $SNAT_NAME
+    fi
+
+    create_ip_table_rule_if_needed "-t nat" "-A $SNAT_NAME -j SNAT --to-source $VIP"
+}
+
+eval $NS ip link show
 if [ $? -ne 0 ]; then
-    ip link add $OUTER_DEV type veth peer name $INNER_DEV
+    ip netns add $NS_NAME
     exit_on_error
 fi
 
-ip link set $OUTER_DEV up
-exit_on_error
+delete_orphan_outer_dev $NS_NAME $PUB_IDEV $PUB_ODEV
+delete_orphan_outer_dev $NS_NAME $PRI_IDEV $PRI_ODEV
 
-# add the outter ethdev to the public bridge
-brctl show $PUB_BR | grep $OUTER_DEV > /dev/null
-if [ $? -ne 0 ]; then
-    brctl addif $PUB_BR $OUTER_DEV
-    exit_on_error
-fi
+create_dev_if_needed $PUB_ODEV $PUB_IDEV
+create_dev_if_needed $PRI_ODEV $PRI_IDEV
 
-NS="ip netns exec $BR_NAME"
+add_dev_to_br_if_needed $PUB_BR $PUB_ODEV
+add_dev_to_br_if_needed $PRI_BR $PRI_ODEV
 
-# add the inner ethdev to the namespace
-eval $NS ip link | grep $INNER_DEV > /dev/null
-if [ $? -ne 0 ]; then
-    ip link set $INNER_DEV netns $BR_NAME
-    exit_on_error
-fi
+add_dev_to_namespace_if_needed $PUB_IDEV $NS_NAME
+add_dev_to_namespace_if_needed $PRI_IDEV $NS_NAME
 
-# set VIP to the inner dev
-eval $NS ip addr show $INNER_DEV | grep $DHCP_IP > /dev/null
-if [ $? -ne 0 ]; then
-    eval $NS ip addr flush dev $INNER_DEV
-    exit_on_error
-    eval $NS ip addr add $VIP/$VIP_NETMASK dev $INNER_DEV
-    exit_on_error
-fi
-
-# set the inner dev up
-eval $NS ip link set $INNER_DEV up
-exit_on_error
+set_ip_to_idev_if_needed $PUB_IDEV $VIP $VIP_NETMASK
+set_ip_to_idev_if_needed $PRI_IDEV $NIC_GATEWAY $NIC_NETMASK
 
 # ping VIP gateway
-eval $NS arping -q -U -c 1 -I $INNER_DEV $VIP_GW > /dev/null
+eval $NS arping -q -U -c 1 -I $PUB_IDEV $VIP_GW > /dev/null
 
-DNAT_NAME="DNAT-$VIP"
-eval $NS iptables-save | grep "$DNAT_NAME" > /dev/null
-if [ $? -ne 0 ]; then
-    eval $NS iptables -N $DNAT_NAME
-    eval $NS iptables -t nat -I PREROUTING -d $VIP -j $DNAT_NAME
-fi
-
-FWD_NAME="FWD-$VIP"
-eval $NS iptables-save | grep "$FWD_NAME" > /dev/null
-if [ $? -ne 0 ]; then
-    eval $NS iptables -N $FWD_NAME
-    eval $NS iptables -I PREROUTING -d $VIP -j $DNAT_NAME
-fi
+block_arp_for_nic_gw
+set_eip_rules
 
 exit 0
 '''
 
         for eip in eips:
             ctx = {
-                "odev": "%s_o" % eip.vip.replace(".", ""),
-                "idev": "%s_i" % eip.vip.replace(".", ""),
+                "pub_odev": "%s_o" % eip.vip.replace(".", ""),
+                "pub_idev": "%s_i" % eip.vip.replace(".", ""),
+                "pri_odev": "$s_o" % eip.nicGateway.replace(".", ""),
+                "pri_idev": "$s_i" % eip.nicGateway.replace(".", ""),
+                "ebtable_chain_name": eip.nicGateway,
             }
             ctx.update(eip.__dict__)
             tmpt = Template(create_eip_cmd)
