80096e8e36b197d7631e12fff8e3d0cf6b9e796a
Mei Lei <meilei007@gmail.com>
2016-07-26 15:06:56 +0800

    Add ceph,fusionstore backupstorage reply with image format
    Signed-off-by: Mei Lei <meilei007@gmail.com>

--- fusionstorbackupstorage/fusionstorbackupstorage/fusionstoragent.py
+++ fusionstorbackupstorage/fusionstorbackupstorage/fusionstoragent.py
@@ -188,6 +188,7 @@ class FusionstorAgent(object):
         rsp = DownloadRsp()
         rsp.size = size
         rsp.actualSize = actual_size
+        rsp.format = file_format.strip('\n')
         self._set_capacity_to_response(rsp)
         return jsonobject.dumps(rsp)
 
