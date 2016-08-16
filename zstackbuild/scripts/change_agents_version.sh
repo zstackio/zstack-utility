9104f0e8db12e345af7188247bc81fc47d4c9212
youyk <yongkang.you@gmail.com>
2016-08-16 08:59:51 +0800

    bounce agent to 1.6

--- zstackbuild/scripts/change_agents_version.sh
+++ zstackbuild/scripts/change_agents_version.sh
@@ -1,6 +1,6 @@
 zstackutility=$1
-find $zstackutility -name setup.py -exec sed -i "s#version = '1.0'#version = '1.1'#" {} \;
+find $zstackutility -name setup.py -exec sed -i "s#version = '1.5'#version = '1.6'#" {} \;
 
-#find /root/zstack -name pom.xml -exec sed -i "s/1.0.0/1.1.0/g" {} \;
-#find /root/zstack -name *.java -exec sed -i 's/1.0.tar.gz/1.1.tar.gz/g' {} \;
+#find /root/zstackorg/zstack -name pom.xml -exec sed -i "s/1.5.0/1.6.0/g" {} \;
+#find /root/zstackorg/zstack -name *.java -exec sed -i 's/1.5.tar.gz/1.6.tar.gz/g' {} \;
 
