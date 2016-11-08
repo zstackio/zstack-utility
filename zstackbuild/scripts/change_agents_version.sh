8398b88d8253dba1cfe19c3fbefc7c3518a3ecfb
youyk <yongkang.you@gmail.com>
2016-11-08 08:48:15 +0800

    prepare to bounce to 1.8 branch

--- zstackbuild/scripts/change_agents_version.sh
+++ zstackbuild/scripts/change_agents_version.sh
@@ -1,6 +1,6 @@
 zstackutility=$1
-find $zstackutility -name setup.py -exec sed -i "s#version = '1.5'#version = '1.6'#" {} \;
+find $zstackutility -name setup.py -exec sed -i "s#version = '1.7'#version = '1.8'#" {} \;
 
-#find /root/zstackorg/zstack -name pom.xml -exec sed -i "s/1.5.0/1.6.0/g" {} \;
-#find /root/zstackorg/zstack -name *.java -exec sed -i 's/1.5.tar.gz/1.6.tar.gz/g' {} \;
+find /root/zstackorg/zstack -name pom.xml -exec sed -i "s/1.7.0/1.8.0/g" {} \;
+find /root/zstackorg/zstack -name *.java -exec sed -i 's/1.7.tar.gz/1.8.tar.gz/g' {} \;
 
