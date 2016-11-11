cc9c6b221d074522a8949392ee6b5e1927298d06
mingjian2049 <mingjian.deng@mevoco.com>
2016-11-11 10:52:14 +0800

    support ceph ps set-root-password

--- kvmagent/kvmagent/plugins/vm_plugin.py
+++ kvmagent/kvmagent/plugins/vm_plugin.py
@@ -11,7 +11,7 @@ import libvirt
 import zstacklib.utils.iptables as iptables
 import zstacklib.utils.lock as lock
 from kvmagent import kvmagent
-from kvmagent.plugins import generate_passwd
+from zstacklib.utils import generate_passwd
 from zstacklib.utils import http
 from zstacklib.utils import jsonobject
 from zstacklib.utils import lichbd
