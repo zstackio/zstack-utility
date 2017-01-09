10ebedba7bdb3af9183e49f9c66054fb95fa64e3
Mei Lei <meilei007@gmail.com>
2017-01-09 16:11:26 +0800

    Add support for collect host/bs last 15 days logs
    for https://github.com/zxwing/premium/issues/1605
    
    Signed-off-by: Mei Lei <meilei007@gmail.com>

--- zstackctl/zstackctl/zstacklib.py
+++ zstackctl/zstackctl/zstacklib.py
@@ -203,7 +203,7 @@ def create_log(logger_dir, logger_file):
     logger.setLevel(logging.DEBUG)
     fmt = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     handler = logging.handlers.RotatingFileHandler(logger_dir + '/' + logger_file, maxBytes=10 * 1024 * 1024,
-                                                   backupCount=10)
+                                                   backupCount=30)
     handler.setFormatter(fmt)
     logger.addHandler(handler)
 
