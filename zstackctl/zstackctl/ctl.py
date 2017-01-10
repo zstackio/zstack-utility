b101b621c94a2320069488027e5ef158c511511c
miao.DengSheng <xuexuemiao@yeah.net>
2017-01-10 14:39:26 +0800

    set log_bin_trust_function_creators for triggers

--- zstackctl/zstackctl/ctl.py
+++ zstackctl/zstackctl/ctl.py
@@ -32,6 +32,63 @@ import itertools
 import platform
 from  datetime import datetime, timedelta
 
+mysql_db_config_script='''
+echo "modify my.cnf"
+if [ -f /etc/mysql/mariadb.conf.d/50-server.cnf ]; then
+    #ubuntu 16.04
+    mysql_conf=/etc/mysql/mariadb.conf.d/50-server.cnf
+elif [ -f /etc/mysql/my.cnf ]; then
+    # Ubuntu 14.04
+    mysql_conf=/etc/mysql/my.cnf
+elif [ -f /etc/my.cnf ]; then
+    # centos
+    mysql_conf=/etc/my.cnf
+fi
+
+sed -i 's/^bind-address/#bind-address/' $mysql_conf
+sed -i 's/^skip-networking/#skip-networking/' $mysql_conf
+sed -i 's/^bind-address/#bind-address/' $mysql_conf
+
+grep 'binlog_format=' $mysql_conf >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "binlog_format=mixed"
+    sed -i '/\[mysqld\]/a binlog_format=mixed\' $mysql_conf
+fi
+
+grep 'log_bin_trust_function_creators=' $mysql_conf >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "log_bin_trust_function_creators=1"
+    sed -i '/\[mysqld\]/a log_bin_trust_function_creators=1\' $mysql_conf
+fi
+
+grep 'expire_logs=' $mysql_conf >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "expire_logs=30"
+    sed -i '/\[mysqld\]/a expire_logs=30\' $mysql_conf
+fi
+
+grep 'max_binlog_size=' $mysql_conf >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "max_binlog_size=500m"
+    sed -i '/\[mysqld\]/a max_binlog_size=500m\' $mysql_conf
+fi
+
+grep 'log-bin=' $mysql_conf >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "log-bin=mysql-binlog"
+    sed -i '/\[mysqld\]/a log-bin=mysql-binlog\' $mysql_conf
+fi
+
+grep '^character-set-server' $mysql_conf >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "binlog_format=mixed"
+    sed -i '/\[mysqld\]/a character-set-server=utf8\' $mysql_conf
+fi
+grep '^skip-name-resolve' $mysql_conf >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    sed -i '/\[mysqld\]/a skip-name-resolve\' $mysql_conf
+fi
+'''
 
 def signal_handler(signal, frame):
     sys.exit(0)
@@ -1054,57 +1111,8 @@ class DeployDBCmd(Command):
         ctl.register_command(self)
 
     def update_db_config(self):
-        update_db_config_script = '''
-echo "modify my.cnf"
-if [ -f /etc/mysql/mariadb.conf.d/50-server.cnf ]; then
-    #ubuntu 16.04
-    mysql_conf=/etc/mysql/mariadb.conf.d/50-server.cnf
-elif [ -f /etc/mysql/my.cnf ]; then
-    # Ubuntu 14.04
-    mysql_conf=/etc/mysql/my.cnf
-elif [ -f /etc/my.cnf ]; then
-    # centos
-    mysql_conf=/etc/my.cnf
-fi
+        update_db_config_script = mysql_db_config_script
 
-sed -i 's/^bind-address/#bind-address/' $mysql_conf
-sed -i 's/^skip-networking/#skip-networking/' $mysql_conf
-sed -i 's/^bind-address/#bind-address/' $mysql_conf
-
-grep 'binlog_format=' $mysql_conf
-if [ $? -ne 0 ]; then
-    echo "binlog_format=mixed"
-    sed -i '/\[mysqld\]/a binlog_format=mixed\' $mysql_conf
-fi
-
-grep 'expire_logs=' $mysql_conf
-if [ $? -ne 0 ]; then
-    echo "expire_logs=30"
-    sed -i '/\[mysqld\]/a expire_logs=30\' $mysql_conf
-fi
-
-grep 'max_binlog_size=' $mysql_conf
-if [ $? -ne 0 ]; then
-    echo "max_binlog_size=500m"
-    sed -i '/\[mysqld\]/a max_binlog_size=500m\' $mysql_conf
-fi
-
-grep 'log-bin=' $mysql_conf
-if [ $? -ne 0 ]; then
-    echo "log-bin=mysql-binlog"
-    sed -i '/\[mysqld\]/a log-bin=mysql-binlog\' $mysql_conf
-fi
-
-grep '^character-set-server' $mysql_conf
-if [ $? -ne 0 ]; then
-    echo "binlog_format=mixed"
-    sed -i '/\[mysqld\]/a character-set-server=utf8\' $mysql_conf
-fi
-grep '^skip-name-resolve' $mysql_conf
-if [ $? -ne 0 ]; then
-    sed -i '/\[mysqld\]/a skip-name-resolve\' $mysql_conf
-fi
-'''
         fd, update_db_config_script_path = tempfile.mkstemp()
         os.fdopen(fd, 'w').write(update_db_config_script)
         info('update_db_config_script_path is: %s' % update_db_config_script_path)
@@ -1889,57 +1897,7 @@ exit 1
 
         self.install_cleanup_routine(cleanup_pre_install_script)
 
-        post_install_script = '''
-echo "modify my.cnf"
-if [ -f /etc/mysql/mariadb.conf.d/50-server.cnf ]; then
-    #ubuntu 16.04
-    mysql_conf=/etc/mysql/mariadb.conf.d/50-server.cnf
-elif [ -f /etc/mysql/my.cnf ]; then
-    # Ubuntu 14.04
-    mysql_conf=/etc/mysql/my.cnf
-elif [ -f /etc/my.cnf ]; then
-    # centos
-    mysql_conf=/etc/my.cnf
-fi
-
-sed -i 's/^bind-address/#bind-address/' $mysql_conf
-sed -i 's/^skip-networking/#skip-networking/' $mysql_conf
-sed -i 's/^bind-address/#bind-address/' $mysql_conf
-
-grep 'binlog_format=' $mysql_conf >/dev/null 2>&1
-if [ $? -ne 0 ]; then
-    echo "binlog_format=mixed"
-    sed -i '/\[mysqld\]/a binlog_format=mixed\' $mysql_conf
-fi
-
-grep 'expire_logs=' $mysql_conf >/dev/null 2>&1
-if [ $? -ne 0 ]; then
-    echo "expire_logs=30"
-    sed -i '/\[mysqld\]/a expire_logs=30\' $mysql_conf
-fi
-
-grep 'max_binlog_size=' $mysql_conf >/dev/null 2>&1
-if [ $? -ne 0 ]; then
-    echo "max_binlog_size=500m"
-    sed -i '/\[mysqld\]/a max_binlog_size=500m\' $mysql_conf
-fi
-
-grep 'log-bin=' $mysql_conf >/dev/null 2>&1
-if [ $? -ne 0 ]; then
-    echo "log-bin=mysql-binlog"
-    sed -i '/\[mysqld\]/a log-bin=mysql-binlog\' $mysql_conf
-fi
-
-grep '^character-set-server' $mysql_conf >/dev/null 2>&1
-if [ $? -ne 0 ]; then
-    echo "binlog_format=mixed"
-    sed -i '/\[mysqld\]/a character-set-server=utf8\' $mysql_conf
-fi
-grep '^skip-name-resolve' $mysql_conf >/dev/null 2>&1
-if [ $? -ne 0 ]; then
-    sed -i '/\[mysqld\]/a skip-name-resolve\' $mysql_conf
-fi
-'''
+        post_install_script = mysql_db_config_script
         fd, post_install_script_path = tempfile.mkstemp()
         os.fdopen(fd, 'w').write(post_install_script)
 
