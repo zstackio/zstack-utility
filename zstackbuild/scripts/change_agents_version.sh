6170f060c738ebebb2b1f120e35492c51f243337
youyk <yongkang.you@gmail.com>
2017-02-07 19:45:13 +0800

    bounce all agents' version from 1.8 to 1.9

--- zstackbuild/scripts/change_agents_version.sh
+++ zstackbuild/scripts/change_agents_version.sh
@@ -1,6 +1,8 @@
-zstackutility=$1
-find $zstackutility -name setup.py -exec sed -i "s#version = '1.7'#version = '1.8'#" {} \;
+zstackutility_folder=$1
+zstack_folder=$2
+find $zstackutility_folder -name setup.py -exec sed -i "s#version = '1.8'#version = '1.9'#" {} \;
 
-find /root/zstackorg/zstack -name pom.xml -exec sed -i "s/1.7.0/1.8.0/g" {} \;
-find /root/zstackorg/zstack -name *.java -exec sed -i 's/1.7.tar.gz/1.8.tar.gz/g' {} \;
+#after executing the script, you need to double check 'git diff' to make sure all changes are correct. Then use git commit in zstack folder and premium folder.
+find $zstack_folder -name pom.xml -exec sed -i "s/1.8.0/1.9.0/g" {} \;
+find $zstack_folder -name *.java -exec sed -i 's/1.8.tar.gz/1.9.tar.gz/g' {} \;
 
