2b0e1d9fd40aa3278d301e872d1adc6dfb87e435
Guo Yi <yi.guo@mevoco.com>
2017-03-10 07:20:19 -0500

    Fix Bug #2499: ZSTACK-ENTERPRISE is too long for FIGlet

--- zstackbuild/scripts/gen_installation_title.sh
+++ zstackbuild/scripts/gen_installation_title.sh
@@ -4,7 +4,9 @@ output_file=$3
 
 which figlet
 if [ $? -eq 0 ]; then
-    figlet -w 80 $product |tee -a $output_file
+    # handle long product name
+    # only works for zstack-enterprise etc.
+    figlet -w 80 `echo $product | sed 's/-/\n/g'` |tee -a $output_file
     figlet -w 80 v $version |tee -a $output_file
     figlet -w 80 INSTALLATION |tee -a $output_file
 fi
