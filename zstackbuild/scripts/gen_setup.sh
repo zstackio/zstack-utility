134ba013af36b4a912503e999148717866f3ecee
Guo Yi <yi.guo@mevoco.com>
2017-03-11 03:35:31 -0500

    Fix Bug #2505
    Update repo version check strategy due to PRODUCT_NAME updates

--- zstackbuild/scripts/gen_setup.sh
+++ zstackbuild/scripts/gen_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat >$3 <<EOF
+cat >$4 <<EOF
 #!/bin/bash
 line=\`wc -l \$0|awk '{print \$1}'\`
 line=\`expr \$line - 11\`
@@ -8,7 +8,7 @@ tmpdir=\`mktemp\`
 mkdir -p \$tmpdir
 tail -n \$line \$0 |tar x -C \$tmpdir
 cd \$tmpdir
-PRODUCT_NAME=$1 PRODUCT_VERSION=$2 bash ./install.sh -a -f zstack*.tgz \$*
+PRODUCT_NAME=$1 PRODUCT_VERSION=$2 CHECK_REPO_VERSION=$3 bash ./install.sh -a -f zstack*.tgz \$*
 ret=\$?
 rm -rf \$tmpdir
 exit \$ret
