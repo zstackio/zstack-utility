81a92ca35e29afdc9939716921124a6454518eaa
miao.DengSheng <xuexuemiao@yeah.net>
2017-06-06 15:17:29 +0800

    fix kill_vm()

--- kvmagent/kvmagent/plugins/ha_plugin.py
+++ kvmagent/kvmagent/plugins/ha_plugin.py
@@ -6,7 +6,6 @@ from zstacklib.utils import shell
 from zstacklib.utils import linux
 from zstacklib.utils import thread
 import os.path
-import re
 import time
 import traceback
 
@@ -24,8 +23,16 @@ class ScanRsp(object):
 
 
 def kill_vm(maxAttempts, mountPath = None, isFileSystem = None):
-    vm_uuid_list = shell.call("virsh list | grep running | awk '{print $2}'")
-    for vm_uuid in vm_uuid_list.split('\n'):
+    zstack_uuid_pattern = "'[0-9a-f]{8}[0-9a-f]{4}[1-5][0-9a-f]{3}[89ab][0-9a-f]{3}[0-9a-f]{12}'"
+
+    virsh_list = shell.call("virsh list --all")
+    logger.debug("virsh_list:\n" + virsh_list)
+
+    vm_in_process_uuid_list = shell.call("virsh list | egrep -o " + zstack_uuid_pattern + " | sort | uniq")
+    logger.debug('vm_in_process_uuid_list:\n' + vm_in_process_uuid_list)
+
+    # kill vm's qemu process
+    for vm_uuid in vm_in_process_uuid_list.split('\n'):
         vm_uuid = vm_uuid.strip(' \t\n\r')
         if not vm_uuid:
             continue
@@ -177,6 +184,7 @@ class HaPlugin(kvmagent.KvmAgent):
 
                 logger.debug('stop self-fencer on ceph primary storage')
             except:
+                logger.debug('self-fencer on ceph primary storage stopped abnormally')
                 content = traceback.format_exc()
                 logger.warn(content)
 
