a50a94a86f789ffb154ff3e8b6e1762193838743
Mei Lei <meilei007@gmail.com>
2017-07-03 09:42:21 +0800

    disable spice streaming mode
    Signed-off-by: Mei Lei <meilei007@gmail.com>

--- kvmagent/kvmagent/plugins/vm_plugin.py
+++ kvmagent/kvmagent/plugins/vm_plugin.py
@@ -2582,7 +2582,7 @@ class Vm(object):
             e(spice, "jpeg", None, {'compression': 'always'})
             e(spice, "zlib", None, {'compression': 'never'})
             e(spice, "playback", None, {'compression': 'off'})
-            e(spice, "streaming", None, {'mode': 'filter'})
+            e(spice, "streaming", None, {'mode': 'off'})
             e(spice, "mouse", None, {'mode': 'client'})
             e(spice, "filetransfer", None, {'enable': 'no'})
             e(spice, "clipboard", None, {'copypaste': 'no'})
