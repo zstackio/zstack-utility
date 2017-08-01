d53507c40dcb277ffb8188c2201c86ab020e7717
J M <630911437@qq.com>
2017-08-02 00:05:18 +0800

    fix nfs ping logic

--- kvmagent/kvmagent/plugins/nfs_primarystorage_plugin.py
+++ kvmagent/kvmagent/plugins/nfs_primarystorage_plugin.py
@@ -235,7 +235,7 @@ class NfsPrimaryStoragePlugin(kvmagent.KvmAgent):
         cmd = jsonobject.loads(req[http.REQUEST_BODY])
         mount_path = self.mount_path[cmd.uuid]
         # if nfs service stop, os.path.isdir will hung
-        if not mount_path or (bash_r("timeout 15 ls %s" % mount_path) != 0):
+        if not linux.timeout_isdir(mount_path) or not linux.is_mounted(path=mount_path):
             raise Exception('the mount path[%s] of the nfs primary storage[uuid:%s] is not existing' % (mount_path, cmd.uuid))
 
         test_file = os.path.join(mount_path, '%s-ping-test-file' % uuidhelper.uuid())
