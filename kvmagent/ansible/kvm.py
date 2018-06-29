204fd698b105aed1fff76c0aabf06759dbc37512
GuoYi <yi.guo@zstack.io>
2018-06-29 19:00:44 +0800

    Fixes ZSTAC-13111

--- kvmagent/ansible/kvm.py
+++ kvmagent/ansible/kvm.py
@@ -172,7 +172,7 @@ if distro in RPM_BASED_OS:
 
         # name: install kvm related packages on RedHat based OS from user defined repo
         # update some packages if possible
-        command = ("yum --enablerepo=%s clean metadata && pkg_list=`rpm -q %s | grep \"not installed\" | awk '{ print $2 }'`' sanlock hwdata sg3_utils' && for pkg in $pkg_list; do yum --disablerepo=* --enablerepo=%s install -y $pkg; done;") % (zstack_repo, dep_list, zstack_repo)
+        command = ("yum --enablerepo=%s clean metadata >/dev/null && pkg_list=`rpm -q %s | grep \"not installed\" | awk '{ print $2 }'`' sanlock hwdata sg3_utils' && for pkg in $pkg_list; do yum --disablerepo=* --enablerepo=%s install -y $pkg >/dev/null || exit 1; done;") % (zstack_repo, dep_list, zstack_repo)
         host_post_info.post_label = "ansible.shell.install.pkg"
         host_post_info.post_label_param = dep_list
         run_remote_command(command, host_post_info)
