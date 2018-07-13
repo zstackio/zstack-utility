3ead0ef0a665f1c1251874d670f9dc9d536f611e
Qun Li <qun.li@zstack.io>
2018-07-13 16:38:52 +0800

    fix the handling of 'parameters' for scheduler job
    Signed-off-by: Qun Li <qun.li@zstack.io>

--- zstackcli/zstackcli/cli.py
+++ zstackcli/zstackcli/cli.py
@@ -337,6 +337,8 @@ Parse command parameters error:
                     all_params[params[0]] = eval_string(params[0], params[1])
                 elif apiname == 'APIDetachNetworkServiceFromL3NetworkMsg' and params[0] == 'networkServices':
                     all_params[params[0]] = eval_string(params[0], params[1])
+                elif apiname == 'APICreateSchedulerJobMsg' and params[0] == 'parameters':
+                    all_params[params[0]] = eval_string(params[0], params[1])
                 elif apiname == 'APICreatePolicyMsg' and params[0] == 'statements':
                     all_params[params[0]] = eval_string(params[0], params[1])
                 elif is_api_param_a_list(apiname, params[0]):
