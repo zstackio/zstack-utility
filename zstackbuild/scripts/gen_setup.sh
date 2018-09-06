98f1b0943b1f5a050c84a6abade78674fbb7c850
GuoYi <yi.guo@zstack.io>
2018-09-06 19:25:08 +0800

    remove flock in gen_setup.sh

--- zstackbuild/scripts/gen_setup.sh
+++ zstackbuild/scripts/gen_setup.sh
@@ -2,21 +2,14 @@
 cat >$4 <<EOF
 #!/bin/bash
 line=\`wc -l \$0|awk '{print \$1}'\`
-line=\`expr \$line - 18\`
+line=\`expr \$line - 11\`
 tmpdir=\`mktemp\`
 /bin/rm -f \$tmpdir
 mkdir -p \$tmpdir
 tail -n \$line \$0 |tar x -C \$tmpdir
 cd \$tmpdir
-(
-  flock -n 9
-  if [ \$? -ne 0 ]; then
-    echo "There is another ZStack installation progress running."
-    exit 1;
-  fi
-  PRODUCT_NAME=$1 PRODUCT_VERSION=$2 CHECK_REPO_VERSION=$3 bash ./install.sh -o -f zstack*.tgz \$*
-) 9>/tmp/zstack_lock_file
+PRODUCT_NAME=$1 PRODUCT_VERSION=$2 CHECK_REPO_VERSION=$3 bash ./install.sh -o -f zstack*.tgz \$*
 ret=\$?
-rm -rf \$tmpdir /tmp/zstack_lock_file
+rm -rf \$tmpdir
 exit \$ret
 EOF
