8a8d0b86525df8941d54e7dd9eab3688ea30f82f
yaohua.wu <yaohua.wu@zstack.io>
2018-11-02 13:37:58 +0800

    Resolves ZSTAC-16004

---
+++ zstackctl/zstackctl/utils/shell.py
@@ -0,0 +1,35 @@
+import subprocess
+
+
+class CtlError(Exception):
+    pass
+    
+class ShellCmd(object):
+    def __init__(self, cmd, workdir=None, pipe=True):
+        self.cmd = cmd
+        if pipe:
+            self.process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE,
+                                            stderr=subprocess.PIPE, close_fds=True, executable='/bin/bash', cwd=workdir)
+        else:
+            self.process = subprocess.Popen(cmd, shell=True, close_fds=True, executable='/bin/bash', cwd=workdir)
+            
+        self.stdout = None
+        self.stderr = None
+        self.return_code = None
+
+    def raise_error(self):
+        err = []
+        err.append('failed to execute shell command: %s' % self.cmd)
+        err.append('return code: %s' % self.process.returncode)
+        err.append('stdout: %s' % self.stdout)
+        err.append('stderr: %s' % self.stderr)
+        raise CtlError('\n'.join(err))
+        
+    def __call__(self, is_exception=True):
+
+        (self.stdout, self.stderr) = self.process.communicate()
+        if is_exception and self.process.returncode != 0:
+            self.raise_error()
+
+        self.return_code = self.process.returncode
+        return self.stdout
