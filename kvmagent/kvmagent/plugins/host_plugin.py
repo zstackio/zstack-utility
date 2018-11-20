3a2a8ddaf751489a9a0c1eeaf3eaa08142c2c153
GuoYi <yi.guo@zstack.io>
2018-11-20 16:57:14 +0800

    fix bug when getting arm cpu MHz

--- kvmagent/kvmagent/plugins/host_plugin.py
+++ kvmagent/kvmagent/plugins/host_plugin.py
@@ -316,10 +316,6 @@ class HostPlugin(kvmagent.KvmAgent):
         ipV4Addrs = shell.call("ip addr | grep -w inet | grep -v 127.0.0.1 | awk '!/zs$/{print $2}' | cut -d/ -f1")
         system_product_name = shell.call('dmidecode -s system-product-name').strip()
         baseboard_product_name = shell.call('dmidecode -s baseboard-product-name').strip()
-        host_cpu_info = shell.call("grep -m2 -P -o '(model name|cpu MHz)\s*:\s*\K.*' /proc/cpuinfo").splitlines()
-        host_cpu_model_name = host_cpu_info[0]
-        transient_cpuGHz = '%.2f' % (float(host_cpu_info[1]) / 1000)
-        static_cpuGHz_re = re.search('[0-9.]*GHz', host_cpu_model_name)
 
         rsp.qemuImgVersion = qemu_img_version
         rsp.libvirtVersion = self.libvirt_version
@@ -327,8 +323,7 @@ class HostPlugin(kvmagent.KvmAgent):
         rsp.systemProductName = system_product_name if system_product_name else baseboard_product_name
         if not rsp.systemProductName:
             rsp.systemProductName = 'unknown'
-        rsp.hostCpuModelName = host_cpu_model_name
-        rsp.cpuGHz = static_cpuGHz_re.group(0)[:-3] if static_cpuGHz_re else transient_cpuGHz
+
         if IS_AARCH64:
             # FIXME how to check vt of aarch64?
             rsp.hvmCpuFlag = 'vt'
@@ -341,6 +336,10 @@ class HostPlugin(kvmagent.KvmAgent):
                     cpu_model = shell.call('uname -p').strip("\t\r\n")
 
             rsp.cpuModelName = cpu_model
+            rsp.hostCpuModelName = "aarch64"
+
+            cpuMHz = shell.call("lscpu | grep 'max MHz' | awk '{ print $NF }'")
+            rsp.cpuGHz = '%.2f' % (float(cpuMHz) / 1000)
         else:
             if shell.run('grep vmx /proc/cpuinfo') == 0:
                 rsp.hvmCpuFlag = 'vmx'
@@ -350,6 +349,14 @@ class HostPlugin(kvmagent.KvmAgent):
                     rsp.hvmCpuFlag = 'svm'
 
             rsp.cpuModelName = self._get_host_cpu_model()
+
+            host_cpu_info = shell.call("grep -m2 -P -o '(model name|cpu MHz)\s*:\s*\K.*' /proc/cpuinfo").splitlines()
+            host_cpu_model_name = host_cpu_info[0]
+            rsp.hostCpuModelName = host_cpu_model_name
+
+            transient_cpuGHz = '%.2f' % (float(host_cpu_info[1]) / 1000)
+            static_cpuGHz_re = re.search('[0-9.]*GHz', host_cpu_model_name)
+            rsp.cpuGHz = static_cpuGHz_re.group(0)[:-3] if static_cpuGHz_re else transient_cpuGHz
 
         return jsonobject.dumps(rsp)
 
