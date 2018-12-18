4e51de13e79d53904b2b0399397b438ff887fb21
AlanJager <alanjager@outlook.com>
2018-12-18 15:58:05 +0800

    Temproarily disable numa in arrch64
    Resolves ZSTAC-17400
    
    Signed-off-by: AlanJager <alanjager@outlook.com>

--- kvmagent/kvmagent/plugins/vm_plugin.py
+++ kvmagent/kvmagent/plugins/vm_plugin.py
@@ -2748,7 +2748,7 @@ class Vm(object):
             elements['root'] = root
 
         def make_cpu():
-            if use_numa:
+            if use_numa and not IS_AARCH64:
                 root = elements['root']
                 e(root, 'vcpu', '128', {'placement': 'static', 'current': str(cmd.cpuNum)})
                 # e(root,'vcpu',str(cmd.cpuNum),{'placement':'static'})
@@ -2804,7 +2804,7 @@ class Vm(object):
         def make_memory():
             root = elements['root']
             mem = cmd.memory / 1024
-            if use_numa:
+            if use_numa and not IS_AARCH64:
                 e(root, 'maxMemory', str(68719476736), {'slots': str(16), 'unit': 'KiB'})
                 # e(root,'memory',str(mem),{'unit':'k'})
                 e(root, 'currentMemory', str(mem), {'unit': 'k'})
@@ -3944,6 +3944,11 @@ class VmPlugin(kvmagent.KvmAgent):
         cmd = jsonobject.loads(req[http.REQUEST_BODY])
         rsp = IncreaseMemoryResponse()
 
+        if IS_AARCH64:
+            rsp.error = 'increase memory of vm[uuid:%s] on arrch64 is not supported yet' % cmd.vmUuid
+            rsp.success = False
+            return jsonobject.dumps(rsp)
+
         try:
             vm = get_vm_by_uuid(cmd.vmUuid)
             memory_size = cmd.memorySize
@@ -3962,6 +3967,11 @@ class VmPlugin(kvmagent.KvmAgent):
     def online_increase_cpu(self, req):
         cmd = jsonobject.loads(req[http.REQUEST_BODY])
         rsp = IncreaseCpuResponse()
+
+        if IS_AARCH64:
+            rsp.error = 'increase cpu of vm[uuid:%s] on arrch64 is not supported yet' % cmd.vmUuid
+            rsp.success = False
+            return jsonobject.dumps(rsp)
 
         try:
             vm = get_vm_by_uuid(cmd.vmUuid)
