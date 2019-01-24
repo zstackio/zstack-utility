8448fc97ae26910c05b5a739f3bf8ae32a45dee1
yaohua.wu <yaohua.wu@zstack.io>
2019-01-24 17:24:12 +0800

    update config when zstack db is upgraded

--- zstackctl/zstackctl/ctl.py
+++ zstackctl/zstackctl/ctl.py
@@ -6714,6 +6714,15 @@ class UpgradeDbCmd(Command):
             info('Dry run finished. Database could be upgraded. ')
             return True
 
+        def update_db_config():
+            update_db_config_script = mysql_db_config_script
+
+            fd, update_db_config_script_path = tempfile.mkstemp()
+            os.fdopen(fd, 'w').write(update_db_config_script)
+            info('update_db_config_script_path is: %s' % update_db_config_script_path)
+            ShellCmd('bash %s' % update_db_config_script_path)()
+            os.remove(update_db_config_script_path)
+
         def backup_current_database():
             if args.no_backup:
                 return
@@ -6759,6 +6768,7 @@ class UpgradeDbCmd(Command):
 
             info('Successfully upgraded the database to the latest version.\n')
 
+        update_db_config()
         backup_current_database()
         create_schema_version_table_if_needed()
         migrate()
