6f68fcb56ec2526bdf6624c32c94aca0a772c7a7
dongjialong <dongjialong0311@163.com>
2019-07-10 03:46:41 +0800

    add mini_auto_check

--- installation/install.sh
+++ installation/install.sh
@@ -3435,10 +3435,12 @@ fi
 if [ x"$MINI_INSTALL" = x"y" ];then
     bash $ZSTACK_INSTALL_ROOT/$CATALINA_ZSTACK_CLASSES/ansible/zsnagentansible/zsn-agent.bin
     cp -f $ZSTACK_INSTALL_ROOT/zsn-agent/bin/zstack-network-agent /etc/init.d/
-    cp -f /opt/zstack-dvd/mini_auto_check /etc/init.d/
+    cp /opt/zstack-dvd/mini_auto_check /etc/init.d/
     chkconfig zstack-network-agent on
-    chkconfig mini_auto_check on
-    echo "echo > /etc/motd" >> /etc/profile
+    chmod +x /etc/init.d/mini_auto_check
+    echo "[ -f /etc/init.d/mini_auto_check ] && bash /etc/init.d/mini_auto_check" >> /etc/rc.local
+    echo "[ -f /etc/issue.bak ] && mv /etc/issue.bak /etc/issue" >> /etc/profile
+    cp /etc/issue /etc/issue.bak
 fi
 
 #Print all installation message
