2bf901904bdb409262ce0a1e2025f36e93a57deb
jialong.dong <jialong.dong@zstack.io>
2019-09-25 10:52:26 +0800

    fix ZSTAC-23506

--- baremetalpxeserver/ansible/baremetalpxeserver.py
+++ baremetalpxeserver/ansible/baremetalpxeserver.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import argparse
-import platform
 from zstacklib import *
 import os
 
@@ -110,7 +109,7 @@ else:
 
 # name: check and mount /opt/zstack-dvd
 command = """
-basearch=`uname -m`;releasever=cat /etc/zstack-release |awk '{print $3}';
+basearch=`uname -m`;releasever=`awk '{print $3}' /etc/zstack-release`;
 [ -f /opt/zstack-dvd/$basearch/$releasever/GPL ] || exit 1;
 mkdir -p /var/lib/zstack/baremetal/{dnsmasq,ftp/{ks,zstack-dvd,scripts},tftpboot/{zstack,pxelinux.cfg},vsftpd} /var/log/zstack/baremetal/;
 cp -f /usr/share/syslinux/pxelinux.0 /var/lib/zstack/baremetal/tftpboot/;
