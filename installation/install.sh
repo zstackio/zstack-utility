6d88d889786e2f0ba02d42a1f73998b6234aaa03
AlanJager <alanjager@outlook.com>
2019-09-25 18:52:25 +0800

    Check getopt return code
    Resolves http://jira.zstack.io/browse/ZSTAC-23430
    
    Signed-off-by: AlanJager <alanjager@outlook.com>

--- installation/install.sh
+++ installation/install.sh
@@ -3022,7 +3022,10 @@ check_myarg() {
 }
 
 OPTIND=1
-TEMP=`getopt -o f:H:I:n:p:P:r:R:t:y:acC:L:T:dDEFhiklmMNoOqsuz --long mini,SY -- "$@" 2>/dev/null`
+TEMP=`getopt -o f:H:I:n:p:P:r:R:t:y:acC:L:T:dDEFhiklmMNoOqsuz --long mini,SY -- "$@"`
+if [ $? != 0 ]; then
+    usage
+fi
 eval set -- "$TEMP"
 while :
 do
