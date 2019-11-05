1c2972b1a4bf65d4fa8ed4bdcafb514d4c8d872c
AlanJager <alanjager@outlook.com>
2019-11-05 22:20:43 +0800

    Add singleton queue for vm plugin
    Signed-off-by: AlanJager <alanjager@outlook.com>

--- kvmagent/kvmagent/plugins/vm_plugin.py
+++ kvmagent/kvmagent/plugins/vm_plugin.py
@@ -38,6 +38,7 @@ from zstacklib.utils import xmlobject
 from zstacklib.utils import misc
 from zstacklib.utils import qemu_img
 from zstacklib.utils.report import *
+from zstacklib.utils.vm_plugin_queue_singleton import VmPluginQueueSingleton
 
 logger = log.get_logger(__name__)
 
@@ -3754,7 +3755,7 @@ class Vm(object):
 
 def _stop_world():
     http.AsyncUirHandler.STOP_WORLD = True
-    VmPlugin.queue.put("exit")
+    VmPlugin.queue_singleton.queue.put("exit")
 
 class VmPlugin(kvmagent.KvmAgent):
     KVM_START_VM_PATH = "/vm/start"
@@ -3814,7 +3815,7 @@ class VmPlugin(kvmagent.KvmAgent):
     VM_OP_RESUME = "resume"
 
     timeout_object = linux.TimeoutObject()
-    queue = Queue.Queue()
+    queue_singleton = VmPluginQueueSingleton()
     secret_keys = {}
 
     if not os.path.exists(QMP_SOCKET_PATH):
@@ -5334,7 +5335,7 @@ class VmPlugin(kvmagent.KvmAgent):
         def wait_end_signal():
             while True:
                 try:
-                    self.queue.get(True)
+                    self.queue_singleton.queue.get(True)
 
                     while http.AsyncUirHandler.HANDLER_COUNTER.get() != 0:
                         time.sleep(0.1)
