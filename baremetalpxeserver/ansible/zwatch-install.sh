50a28a08913a2cbb8d951164acebe0383c936804
shixin.ruan <shixin.ruan@zstack.io>
2019-12-18 14:52:45 +0800

    1. soft link "/var/lib/zstack/kvm/zwatch-vm-agent.linux-amd64.bin" to userdata html /root folder 2. add zwatch-install.sh for baremetal vm

--- baremetalpxeserver/ansible/zwatch-install.sh
+++ baremetalpxeserver/ansible/zwatch-install.sh
@@ -5,6 +5,8 @@ echo "installing zwatch-vm-agent"
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US:
 
+DEPLOY_SERVER=$1
+
 case `uname -s` in
   Linux)
     AGENT_OS="linux"
@@ -25,14 +27,15 @@ BIN_NAME=zwatch-vm-agent.${AGENT_OS}-${ARCH}.bin
 
 NAME=zwatch-vm-agent.${AGENT_OS}-${ARCH}
 
-AGENT_VERSION=`curl -s http://169.254.169.254/agent_version`
+echo "curl -s ftp://$DEPLOY_SERVER/agent_version"
+AGENT_VERSION=`curl -s ftp://$DEPLOY_SERVER/agent_version`
 if [[ ! ${AGENT_VERSION} =~ $NAME ]]; then
     echo "can't get the ${BIN_NAME} latest version"
     exit 1
 fi
 
-echo "downloading ${BIN_NAME}"
-curl http://169.254.169.254/${BIN_NAME} --silent -O
+echo "downloading ${BIN_NAME}: ftp://$DEPLOY_SERVER/${BIN_NAME}"
+curl ftp://$DEPLOY_SERVER/${BIN_NAME} --silent -O
 
 BIN_MD5=`md5sum ${BIN_NAME} | awk '{print $1}'`
 if [[ ! ${AGENT_VERSION} =~ "md5-$NAME=${BIN_MD5}" ]]; then
@@ -53,7 +56,7 @@ fi
 echo "start zwatch-vm-agent"
 service zwatch-vm-agent start
 if [ $? != 0 ]; then
-     echo "service zwatch-vm-agent start fail"
+     echo "start zwatch-vm-agent failed"
      exit 1
 fi
 
