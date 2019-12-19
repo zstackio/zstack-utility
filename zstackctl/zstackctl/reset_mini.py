598440e80f0d73847c0372e77f3a05bcd2379cb3
Wei Wang <wei.wang@zstack.io>
2019-12-20 02:05:13 +0800

    Stop zstack db replicate before install -D

--- zstackctl/zstackctl/reset_mini.py
+++ zstackctl/zstackctl/reset_mini.py
@@ -267,6 +267,7 @@ def cleanup_storage():
 
 @in_bash
 def cleanup_zstack():
+    bash_r("mysql -uroot -pzstack.mysql.password -e 'STOP SLAVE; RESET SLAVE ALL;'")
     bash_r("rm -rf /usr/local/zstack/apache-tomcat/logs/")
     bash_r("rm -rf /var/log/zstack/")
     bash_r("rm -rf /usr/local/zstack")
