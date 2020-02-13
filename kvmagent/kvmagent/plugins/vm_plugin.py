12cebedb5bdb043fd2bfb619a3b96c7732053cc0
Qun Li <qun.li@zstack.io>
2020-02-13 20:34:55 +0800

    qemu-native-tls: fix migration scheme
    Signed-off-by: Qun Li <qun.li@zstack.io>

--- kvmagent/kvmagent/plugins/vm_plugin.py
+++ kvmagent/kvmagent/plugins/vm_plugin.py
@@ -2624,10 +2624,13 @@ class Vm(object):
         destHostIp = cmd.destHostIp
         proto = "tls" if cmd.useTls else "tcp"
         destUrl = "qemu+{1}://{0}/system".format(destHostIp, proto)
-        tcpUri = "{1}://{0}".format(destHostIp, proto)
+        tcpUri = "tcp://{0}".format(destHostIp)
         flag = (libvirt.VIR_MIGRATE_LIVE |
                 libvirt.VIR_MIGRATE_PEER2PEER |
                 libvirt.VIR_MIGRATE_UNDEFINE_SOURCE)
+
+        if proto == 'tls':
+            flag |= libvirt.VIR_MIGRATE_TLS
 
         if cmd.autoConverge:
             flag |= libvirt.VIR_MIGRATE_AUTO_CONVERGE
