319f7420bb708f4fb51527ff9c626253e080ae89
Qun Li <qun.li@zstack.io>
2020-03-03 10:38:39 +0800

    fix bridge mode switch
    Signed-off-by: Qun Li <qun.li@zstack.io>

--- kvmagent/kvmagent/plugins/host_plugin.py
+++ kvmagent/kvmagent/plugins/host_plugin.py
@@ -908,7 +908,7 @@ if __name__ == "__main__":
             rsp.success = False
             rsp.error = "unexpected mode: " + cmd.mode
         else:
-            bash_r("/usr/local/bin/iohub_mocbr.sh %s start", cmd.mode)
+            bash_r("/usr/local/bin/iohub_mocbr.sh %s start" % cmd.mode)
             if cmd.mode == 'mocbr':
                 bash_r("ip link set dev {} master {}".format(cmd.masterVethName, cmd.bridgeName))
         return jsonobject.dumps(rsp)
