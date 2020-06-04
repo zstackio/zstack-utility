ec3dc32e5f7c35f436512703781a779348c3fb9a
JiaLong.Dong <jialong.dong@zstack.io>
2020-06-04 18:48:41 +0800

    [Improvement]

--- zstacklib/ansible/zstacklib.py
+++ zstacklib/ansible/zstacklib.py
@@ -1939,7 +1939,7 @@ enabled=0" >  /etc/yum.repos.d/zstack-experimental-mn.repo
             copy_arg.dest = "/etc/apt/apt.conf"
             copy(copy_arg, host_post_info)
 
-            command = '[ -f /etc/apt/sources.list ] &&  /bin/mv -f /etc/apt/sources.list /etc/apt/sources.list.zstack.%s || true' % datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            command = '[ -f /etc/apt/sources.list ] &&  /bin/mv -f /etc/apt/sources.list /etc/apt/sources.list.bak || true'
             host_post_info.post_label = "ansible.shell.backup.file"
             host_post_info.post_label_param = "/etc/apt/srouces.list"
             run_remote_command(command, host_post_info)
