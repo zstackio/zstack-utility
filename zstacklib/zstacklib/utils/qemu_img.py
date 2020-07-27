3e418554a34d708eaa70137bdb6a20017e43ef36
yaohua.wu <pandawuu@163.com>
2020-07-27 19:47:24 +0800

    [Feature] sblk support shrink snapshot

--- zstacklib/zstacklib/utils/qemu_img.py
+++ zstacklib/zstacklib/utils/qemu_img.py
@@ -1,7 +1,18 @@
 from zstacklib.utils import shell
 from distutils.version import LooseVersion
+import json
 
 __QEMU_IMG_VERSION = None
+
+class CheckResult(object):
+    def __init__(self, offset, t_clusters, check_erorrs, a_clusters, filename, format):
+        self.image_end_offset = offset
+        self.total_clusters = t_clusters
+        self.check_errors = check_erorrs
+        self.allocated_clusters = a_clusters
+        self.filename = filename
+        self.format = format
+
 
 def subcmd(subcmd):
     global __QEMU_IMG_VERSION
@@ -14,3 +25,12 @@ def subcmd(subcmd):
         if subcmd in ['info', 'check', 'compare', 'convert', 'rebase']:
             options += ' --force-share '
     return 'qemu-img %s %s ' % (subcmd, options)
+
+def get_check_result(path):
+    check_cmd = "%s --out json %s" % (subcmd('check'), path)
+    result = json.loads(shell.call(check_cmd))
+    return CheckResult(result.get("image-end-offset"), result.get("total-clusters"),
+                       result.get("check-errors"), result.get("allocated_clusters"),
+                       result.get("filename"), result.get("format"))
+
+
