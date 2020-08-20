3eb844fa086780c506988f4e3dd613e529588bf9
jialong.dong <jialong.dong@zstack.io>
2020-08-20 13:42:35 +0800

    [BugFix: ZSTACK-28860 28872]

--- kvmagent/kvmagent/plugins/vm_plugin.py
+++ kvmagent/kvmagent/plugins/vm_plugin.py
@@ -865,6 +865,68 @@ def get_sgio_value():
     device_name = [x for x in os.listdir("/sys/block") if not x.startswith("loop")][0]
     return "unfiltered" if os.path.isfile("/sys/block/{}/queue/unpriv_sgio".format(device_name)) else "filtered"
 
+class LibvirtEventManager(object):
+    EVENT_DEFINED = "Defined"
+    EVENT_UNDEFINED = "Undefined"
+    EVENT_STARTED = "Started"
+    EVENT_SUSPENDED = "Suspended"
+    EVENT_RESUMED = "Resumed"
+    EVENT_STOPPED = "Stopped"
+    EVENT_SHUTDOWN = "Shutdown"
+
+    event_strings = (
+        EVENT_DEFINED,
+        EVENT_UNDEFINED,
+        EVENT_STARTED,
+        EVENT_SUSPENDED,
+        EVENT_RESUMED,
+        EVENT_STOPPED,
+        EVENT_SHUTDOWN
+    )
+
+    suspend_events = {}
+    suspend_events[0] = "VIR_DOMAIN_EVENT_SUSPENDED_PAUSED"
+    suspend_events[1] = "VIR_DOMAIN_EVENT_SUSPENDED_MIGRATED"
+    suspend_events[2] = "VIR_DOMAIN_EVENT_SUSPENDED_IOERROR"
+    suspend_events[3] = "VIR_DOMAIN_EVENT_SUSPENDED_WATCHDOG"
+    suspend_events[4] = "VIR_DOMAIN_EVENT_SUSPENDED_RESTORED"
+    suspend_events[5] = "VIR_DOMAIN_EVENT_SUSPENDED_FROM_SNAPSHOT"
+    suspend_events[6] = "VIR_DOMAIN_EVENT_SUSPENDED_API_ERROR"
+    suspend_events[7] = "VIR_DOMAIN_EVENT_SUSPENDED_POSTCOPY"
+    suspend_events[8] = "VIR_DOMAIN_EVENT_SUSPENDED_POSTCOPY_FAILED"
+
+    def __init__(self):
+        self.stopped = False
+        libvirt.virEventRegisterDefaultImpl()
+
+        @thread.AsyncThread
+        def run():
+            logger.debug("virEventRunDefaultImpl starts")
+            while not self.stopped:
+                try:
+                    if libvirt.virEventRunDefaultImpl() < 0:
+                        logger.warn("virEventRunDefaultImpl quit with error")
+                except:
+                    content = traceback.format_exc()
+                    logger.warn(content)
+
+            logger.debug("virEventRunDefaultImpl stopped")
+
+        run()
+
+    def stop(self):
+        self.stopped = True
+
+    @staticmethod
+    def event_to_string(index):
+        # type: (int) -> str
+        return LibvirtEventManager.event_strings[index]
+
+    @staticmethod
+    def suspend_event_to_string(index):
+        return LibvirtEventManager.suspend_events[index]
+
+
 class LibvirtAutoReconnect(object):
     conn = libvirt.open('qemu:///system')
 
