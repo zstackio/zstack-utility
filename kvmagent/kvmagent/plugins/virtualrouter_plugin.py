504de50699145129d3017ce630ce99202c4df710
shixin.ruan <shixin.ruan@zstack.io>
2020-09-01 13:52:21 +0800

    [BugFix: ZSTACK-28412] set bootstrap info timeout base mn

--- kvmagent/kvmagent/plugins/virtualrouter_plugin.py
+++ kvmagent/kvmagent/plugins/virtualrouter_plugin.py
@@ -69,6 +69,9 @@ class VirtualRouterPlugin(kvmagent.KvmAgent):
         
         s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1)
+        if cmd.bootStrapInfoTimeout is not None:
+            timeout = int(cmd['bootStrapInfoTimeout'])
+            s.settimeout(timeout)
         buf_size = s.getsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF)
         info_len = len(info)
         if info_len < buf_size:
