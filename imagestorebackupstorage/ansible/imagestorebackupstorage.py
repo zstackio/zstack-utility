722794f1b429c50116758871483e445771d7f43a
jintao.chen <jintao.chen@zstack.io>
2020-09-03 16:32:15 +0800

    [Bugfix: ZSTAC-29285 29244] support hygon kylin v10 sp1

--- imagestorebackupstorage/ansible/imagestorebackupstorage.py
+++ imagestorebackupstorage/ansible/imagestorebackupstorage.py
@@ -100,8 +100,9 @@ if distro in RPM_BASED_OS:
         releasever = get_mn_yum_release()
     x86_64_c74 = "qemu-img-ev fuse-sshfs nmap"
     x86_64_c76 = "qemu-img-ev fuse-sshfs nmap"
-    aarch64_ns10 = "qemu fuse-sshfs nmap"
-    mips64el_ns10 = "qemu-kvm fuse-sshfs nmap"
+    aarch64_ns10 = "qemu-img fuse-sshfs nmap"
+    mips64el_ns10 = "qemu-img-ev fuse-sshfs nmap"
+    x86_64_ns10 = "qemu-img fuse-sshfs nmap"
 
     qemu_pkg = eval("%s_%s" % (host_arch, releasever))
     # skip these packages
