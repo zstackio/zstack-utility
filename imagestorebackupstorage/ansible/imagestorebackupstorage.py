e5d7042248b7281ee6996a835a2ebed3780dd0e6
jintao.chen <jintao.chen@zstack.io>
2020-09-09 13:47:56 +0800

    [BugFix: ZSTACK-29262] use qemu-img-ev

--- imagestorebackupstorage/ansible/imagestorebackupstorage.py
+++ imagestorebackupstorage/ansible/imagestorebackupstorage.py
@@ -104,10 +104,10 @@ if distro in RPM_BASED_OS:
         releasever = 'c74' if output.strip() == 'c72' else output.strip()
     else:
         releasever = get_mn_yum_release()
-    x86_64_c74 = "qemu-kvm-ev fuse-sshfs nmap collectd"
-    x86_64_c76 = "qemu-kvm-ev fuse-sshfs nmap collectd"
-    aarch64_ns10 = "qemu fuse-sshfs nmap collectd"
-    mips64el_ns10 = "qemu-kvm fuse-sshfs nmap collectd"
+    x86_64_c74 = "qemu-img-ev fuse-sshfs nmap collectd"
+    x86_64_c76 = "qemu-img-ev fuse-sshfs nmap collectd"
+    aarch64_ns10 = "qemu-img fuse-sshfs nmap collectd"
+    mips64el_ns10 = "qemu-img-ev fuse-sshfs nmap collectd"
     qemu_pkg = eval("%s_%s" % (host_arch, releasever))
     # skip these packages
     _skip_list = re.split(r'[|;,\s]\s*', skip_packages)
