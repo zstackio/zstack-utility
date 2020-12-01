13be13535eaccf8cacc7b0727b5e76affe96e1c6
AlanJager <alanjager@outlook.com>
2020-12-01 15:05:52 +0800

    [BugFix: ZSTACK-28257]
    Add libvirt closed callback
    
    Make LibvirtEventManager as singleton
    
    Signed-off-by: AlanJager <alanjager@outlook.com>
    
    Conflicts:
    	kvmagent/kvmagent/plugins/vm_plugin.py

---
+++ zstacklib/zstacklib/utils/singleton.py
@@ -0,0 +1,7 @@
+def singleton(cls):
+    instances = {}
+    def wrapper(*args, **kwargs):
+        if cls not in instances:
+          instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+    return wrapper
