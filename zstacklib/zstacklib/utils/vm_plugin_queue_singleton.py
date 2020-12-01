0582f665600132aa5453be32ea2e1949809c9886
AlanJager <alanjager@outlook.com>
2020-12-01 15:05:52 +0800

    [BugFix: ZSTACK-28257]
    Add libvirt closed callback
    
    Make LibvirtEventManager as singleton
    
    Signed-off-by: AlanJager <alanjager@outlook.com>

--- zstacklib/zstacklib/utils/vm_plugin_queue_singleton.py
+++ zstacklib/zstacklib/utils/vm_plugin_queue_singleton.py
@@ -1,14 +1,6 @@
 import Queue
+from zstacklib.utils import singleton
 
-
-class Singleton(object):
-    _instance = None
-
-    def __new__(cls, *args, **kw):
-        if not cls._instance:
-            cls._instance = super(Singleton, cls).__new__(cls, *args)
-        return cls._instance
-
-
-class VmPluginQueueSingleton(Singleton):
+@singleton.singleton
+class VmPluginQueueSingleton(object):
     queue = Queue.Queue()
