4041bb82ff2778e17d2e5974be8d3bdf03262e74
Qun Li <qun.li@zstack.io>
2020-12-28 17:16:54 +0800

    Revert "[BugFix: ZSTACK-33493] fix pool capacity calculation"
    This reverts commit 0fec429880acedeb96c542d1e3e33c5972c99c93.

--- zstacklib/zstacklib/utils/ceph.py
+++ zstacklib/zstacklib/utils/ceph.py
@@ -18,21 +18,8 @@ def is_sandstone():
     return os.path.exists("/opt/sandstone/bin/sds")
 
 
-def parseDfPools(pools):
-    res = {}
-
-    for pool in pools:
-        if not pool.name: continue
-
-        st = pool.stats
-        if st and st.bytes_used and st.max_avail:
-            res[pool.name] = (st.bytes_used, st.max_avail)
-
-    return res
-
-def getCephPoolsCapacity(pools):
+def getCephPoolsCapacity():
     result = []
-    poolDfDict = parseDfPools(pools)
 
     o = shell.call('ceph osd dump -f json')
     df = jsonobject.loads(o)
@@ -139,14 +126,6 @@ def getCephPoolsCapacity(pools):
         if poolCapacity.usedCapacity != 0 and poolCapacity.replicatedSize != 0:
             poolCapacity.usedCapacity = poolCapacity.usedCapacity / poolCapacity.replicatedSize
 
-    for poolCapacity in result:
-        try:
-            bytes_used, max_avail = poolDfDict[poolCapacity.poolName]
-            poolCapacity.usedCapacity = bytes_used
-            poolCapacity.availableCapacity = max_avail
-            poolCapacity.poolTotalSize = max_avail
-        except KeyError:
-            pass
     return result
 
 
