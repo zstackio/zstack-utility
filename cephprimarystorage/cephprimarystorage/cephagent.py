3f86624846a43af70bdff642a1815f4bf2436eb4
lining <2016220101025@std.uestc.edu.cn>
2021-01-22 15:18:01 +0800

    [BugFix: ZSTACK-36182]

--- cephprimarystorage/cephprimarystorage/cephagent.py
+++ cephprimarystorage/cephprimarystorage/cephagent.py
@@ -293,8 +293,7 @@ class CephAgent(plugin.TaskManager):
 
         rsp.totalCapacity = total
         rsp.availableCapacity = avail
-        if ceph.is_xsky():
-            rsp.type = "xsky"
+        rsp.type = ceph.get_ceph_manufacturer()
 
         if not df.pools:
             return
