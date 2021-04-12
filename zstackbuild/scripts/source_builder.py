0d387dbf86ef3808197c437fa35d841bcdb36647
xin.zhang <xin.zhang@zstack.io>
2021-04-12 05:28:33 -0700

    add commands to source_builder.py

--- zstackbuild/scripts/source_builder.py
+++ zstackbuild/scripts/source_builder.py
@@ -1,21 +1,45 @@
 #!/usr/bin/python
-
+import functools
 import os
+import socket
 import subprocess
 import sys
 import tempfile
+import time
 
 import typing
 import argparse
 from pkg_resources import Requirement, Distribution
+import threading
+import simplejson
 
 
 def info(msg):
     sys.stdout.write('INFO: %s\n' % msg)
+    sys.stdout.flush()
 
 
 def warning(msg):
     sys.stdout.write('WARNING: %s\n' % msg)
+    sys.stdout.flush()
+
+
+def error(msg):
+    sys.stderr.write('\n\nERROR: %s\n' % msg)
+    sys.stderr.flush()
+    sys.exit(1)
+
+
+class TimeoutError(Exception):
+    def __init__(self, timeout, cause=None):
+        self.timeout = timeout
+        self.cause = cause
+
+    def __str__(self):
+        if not self.cause:
+            return 'timeout after %s seconds' % self.timeout
+        else:
+            return 'timeout after %s seconds, %s' % (self.timeout, self.cause)
 
 
 class MissingShellCommand(Exception):
@@ -23,16 +47,20 @@ class MissingShellCommand(Exception):
 
 
 class BashError(Exception):
-    def __init__(self, msg, cmd, retcode, stdout, stderr):
+    def __init__(self, msg, cmd, retcode, stdout, stderr, work_dir=None):
         self.msg = msg
         self.cmd = cmd
         self.retcode = retcode
         self.stdout = stdout
         self.stderr = stderr
+        if work_dir is None:
+            self.work_dir = os.getcwd()
+        else:
+            self.work_dir = work_dir
 
     def __str__(self):
-        return 'shell command failure. %s. details [command: %s, retcode:%s, stdout:%s, stderr: %s]' % \
-               (self.msg, self.cmd, self.retcode, self.stdout, self.stderr)
+        return 'shell command failure. %s. details [command: %s, retcode:%s, stdout:%s, stderr: %s, workdir:%s]' % \
+               (self.msg, self.cmd, self.retcode, self.stdout, self.stderr, self.work_dir)
 
 
 def _merge_shell_stdout_stderr(stdout, stderr):
@@ -70,7 +98,7 @@ def call(command, success_code=0, work_dir=None):
 
 
 def call_with_screen_output(cmd, ret_code=0, raise_error=True, work_dir=None):
-    # type: (str, typing.Union[int, list], bool, str) -> None
+    # type: (str, typing.Union[int, list], bool, str) -> int
 
     if work_dir is None:
         print('[BASH]: %s' % cmd)
@@ -92,6 +120,8 @@ def call_with_screen_output(cmd, ret_code=0, raise_error=True, work_dir=None):
 
         if is_err:
             raise BashError(msg='command[%s] failed' % cmd, cmd=cmd, retcode=r, stdout='', stderr='')
+
+    return r
 
 
 def run_with_command_check(command, work_dir=None):
@@ -115,6 +145,24 @@ def run_with_err_msg(command, err_msg, work_dir=None):
     return o
 
 
+def retry(time_out=5, check_interval=1):
+    def wrap(f):
+        @functools.wraps(f)
+        def inner(*args, **kwargs):
+            expired = time.time() + time_out
+            while True:
+                try:
+                    return f(*args, **kwargs)
+                except Exception as e:
+                    if time.time() < expired:
+                        time.sleep(check_interval)
+                    else:
+                        raise TimeoutError(time_out, e)
+
+        return inner
+    return wrap
+
+
 class Builder(object):
     PYPI_DIR = 'zstackbuild/pypi_source/pypi'
     PYPI_URL = 'https://pypi.tuna.tsinghua.edu.cn/simple'
@@ -127,11 +175,23 @@ class Builder(object):
         self.requirements = {}
 
         parser = argparse.ArgumentParser(description='Build pypi repo')
-        parser.add_argument('--dry-run', '-d', action='store_true', dest='dry', default=False, help='only print modification information instead of do the real build')
-        parser.add_argument('--exclude', '-e', action='append', dest='excludes', default=[], help='[Support Multiple] sub projects to be excluded from scan, e.g. -e bm-instance-agent')
-        parser.add_argument('--print', '-p', action='store_true', dest='print_out', default=False, help='only print packages being required')
+        sub_parser = parser.add_subparsers(help='sub-command help', dest="sub_command_name")
+        sp = sub_parser.add_parser(name='print', help='only print packages being required')
+        sp.add_argument('--exclude', '-e', action='append', dest='excludes', default=[], help='[Support Multiple] sub projects to be excluded from scan, e.g. -e bm-instance-agent')
 
-        self.args = parser.parse_args()
+        sp = sub_parser.add_parser(name='build', help='only print packages being required')
+        sp.add_argument('--dry-run', '-d', action='store_true', dest='dry', default=False, help='only print modification information instead of do the real build')
+        sp.add_argument('--exclude', '-e', action='append', dest='excludes', default=[], help='[Support Multiple] sub projects to be excluded from scan, e.g. -e bm-instance-agent')
+
+        sp = sub_parser.add_parser(name='verify', help='verify the pipy repo through a virtualenv')
+        sp.add_argument('--exclude', '-e', action='append', dest='excludes', default=[], help='[Support Multiple] sub projects to be excluded from scan, e.g. -e bm-instance-agent')
+
+        sp = sub_parser.add_parser(name='deps', help='show dependencies of project requirements')
+        sp.add_argument('--exclude', '-e', action='append', dest='excludes', default=[], help='[Support Multiple] sub projects to be excluded from scan, e.g. -e bm-instance-agent')
+        sp.add_argument('--json', '-j', dest='json', default=None, help='the path of file where to output the json result, default is pypi_deps.json in current directory')
+
+        self.args, _ = parser.parse_known_args()
+        self.command = self.args.sub_command_name
 
         self.packages_to_add = {}
 
@@ -152,10 +212,30 @@ class Builder(object):
             self.sub_projects.append((d, setup_py_path))
 
     def _get_project_requires(self, setup_py):
-        info('collecting requirements ...')
-
         tmp_dir = tempfile.mkdtemp()
-        call('python %s egg_info -e %s' % (setup_py, tmp_dir))
+
+        def cleanup():
+            call('rm -rf %s' % tmp_dir)
+
+        setup_py_dir = os.path.dirname(setup_py)
+        cmd = 'python setup.py egg_info -e %s' % tmp_dir
+        r, o, e = run(cmd, work_dir=setup_py_dir)
+        err = _merge_shell_stdout_stderr(o, e)
+        if r != 0 and 'invalid command' in err and 'egg_info' in err:
+            # the package not supporting egg_info which means no dependencies
+            cleanup()
+            return []
+        elif r != 0:
+            cleanup()
+            raise BashError(
+                msg='cannot get requirements from %s' % setup_py,
+                cmd=cmd,
+                retcode=r,
+                stdout=o,
+                stderr=e,
+                work_dir=setup_py_dir
+            )
+
         dirs = os.listdir(tmp_dir)
         assert len(dirs) == 1, '%s contains more than one folder: %s' % (tmp_dir, os.listdir(tmp_dir))
         try:
@@ -167,7 +247,7 @@ class Builder(object):
                 reqs = fd.read().split('\n')
                 return [r.strip(' \t\r') for r in reqs if r.strip(' \t\r')]
         finally:
-            call('rm -rf %s' % tmp_dir)
+            cleanup()
 
     def _collect_requirements(self, project_name, requires):
         for r in requires:
@@ -180,11 +260,12 @@ class Builder(object):
             for f in files:
                 if f.endswith('.tar.gz') or f.endswith('.zip'):
                     egg = f.replace('tar.gz', 'egg').replace('zip', 'egg')
-                    self.existing_packages.append(Distribution.from_filename(egg))
+                    self.existing_packages.append((Distribution.from_filename(egg), os.path.join(root, f)))
 
     def _calculate_modifications(self):
         def is_req_to_add(req):
-            for distro in self.existing_packages:
+            for ep in self.existing_packages:
+                distro, _ = ep
                 if distro in req:
                     return False
 
@@ -243,27 +324,86 @@ class Builder(object):
 
             sys.exit(1)
 
-    def build(self):
+    def _verify(self):
+        def start_http_server():
+            import psutil
+
+            for p in psutil.process_iter():
+                cmdline = p.cmdline()
+                if 'python' in cmdline and 'SimpleHTTPServer' in cmdline and '18008'in cmdline:
+                    call_with_screen_output('kill -9 %s' % p.pid)
+
+            t = threading.Thread(target=lambda : call('python -m SimpleHTTPServer 18008', work_dir=self.PYPI_DIR))
+            t.setDaemon(True)
+            t.start()
+
+        @retry(time_out=5)
+        def wait_for_http_server():
+            so = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            so.settimeout(1)  # 1s
+            so.connect(("127.0.0.1", 18008))
+
+        self._collect_requirements_of_projects()
+        start_http_server()
+        wait_for_http_server()
+
+        tmp_dir = tempfile.mkdtemp()
+
+        def cleanup():
+            call_with_screen_output('rm -rf %s' % tmp_dir)
+
+        call_with_screen_output('virtualenv pypi_verify', work_dir=tmp_dir)
+        venv_dir = os.path.join(tmp_dir, 'pypi_verify')
+        activate_bin_path = os.path.join(venv_dir, 'bin/activate')
+
+        for project_name, reqs in self.requirements.items():
+            info('Verifying requirements in project: %s' % project_name)
+            for r in reqs:
+                ret = call_with_screen_output('source %s && pip install "%s" -i http://127.0.0.1:18008/simple && deactivate' % (activate_bin_path, r), raise_error=False)
+                if ret != 0:
+                    cleanup()
+                    error('failed to install requirement[%s] for project[%s]' % (r, project_name))
+
+        cleanup()
+        info('\n\n\nAll requirements verified successfully')
+
+    def run(self):
         if not os.path.isdir(self.PYPI_DIR):
             raise Exception('%s not found. You must run this script in root folder of zstack-utiltiy' % self.PYPI_DIR)
 
         self._check_tools()
+
+        if self.command == 'build':
+            self._build()
+        elif self.command == 'print':
+            self._print()
+        elif self.command == 'verify':
+            self._verify()
+        elif self.command == 'deps':
+            self._deps()
+
+    def _collect_requirements_of_projects(self):
         self._scan_setup_py()
-        self._generate_existing_package_distribution()
 
         for project_name, setup_py in self.sub_projects:
+            info('collecting requirements ...')
             requires = self._get_project_requires(setup_py)
             self._collect_requirements(project_name, requires)
 
         self._collect_system_requirements()
         self._check_requirements()
 
+    def _print(self):
+        self._collect_requirements_of_projects()
+        self._print_requirements()
+
+    def _build(self):
+        self._collect_requirements_of_projects()
+        self._generate_existing_package_distribution()
         self._calculate_modifications()
 
         if self.args.dry:
             self._print_modification_information()
-        elif self.args.print_out:
-            self._print_requirements()
         else:
             self._do_download()
 
@@ -303,9 +443,89 @@ class Builder(object):
         if r != 0:
             call_with_screen_output('pip install -i %s pip2pi' % self.PYPI_URL)
 
+        try:
+            import psutil
+        except ImportError:
+            call_with_screen_output('pip install -i %s psutil' % self.PYPI_URL)
+
+    def _deps(self):
+        self._collect_requirements_of_projects()
+        self._generate_existing_package_distribution()
+
+        def find_tgz(req):
+            for ep in self.existing_packages:
+                distro, path = ep
+                if distro in req:
+                    return path
+
+            return None
+
+        def find_setup_py(folder):
+            for root, _, files in os.walk(folder):
+                for f in files:
+                    if f == 'setup.py':
+                        return os.path.join(root, f)
+
+            return None
+
+        result = {}
+        for project_name, reqs in self.requirements.items():
+            for r in reqs:
+                tgz_path = find_tgz(r)
+                if tgz_path is None:
+                    error('unable to find any package in %s satisfying %s for project[%s], you may need to run "source_builder build" first' % (self.PYPI_DIR, r, project_name))
+
+                tmp_dir = tempfile.mkdtemp()
+
+                def cleanup():
+                    call('rm -rf %s' % tmp_dir)
+                    pass
+
+                info('collecting dependencies[project:%s, requirement:%s] ...' % (project_name, r))
+                if tgz_path.endswith('.zip'):
+                    call('unzip %s -d %s' % (tgz_path, tmp_dir))
+                else:
+                    call('tar xzf %s -C %s' % (tgz_path, tmp_dir))
+
+                setup_py = find_setup_py(tmp_dir)
+                if setup_py is None:
+                    cleanup()
+                    error('no setup.py found in package[%s], is it broken?' % tgz_path)
+
+                requires = self._get_project_requires(setup_py)
+
+                req_dict = result.get(project_name, {})
+                req_dict[str(r)] = requires
+                result[project_name] = req_dict
+                cleanup()
+
+        print('\n\nDependencies are dumped as below:\n\n')
+        for project_name, req_dict in result.items():
+            print('%s:\n' % project_name)
+            if not req_dict:
+                print('\tNone\n')
+            else:
+                for req_name, deps in req_dict.items():
+                    print('\t%s\n' % req_name)
+                    dep_names = ['\t\t%s' % r for r in deps]
+                    print('%s\n' % '\n'.join(dep_names))
+
+        if self.args.json is not None:
+            result_json = self.args.json
+            dir_name = os.path.dirname(result_json)
+            if not os.path.isdir(dir_name):
+                os.makedirs(dir_name)
+        else:
+            result_json = 'pypi_deps.json'
+
+        with open(result_json, 'w+') as fd:
+            fd.write(simplejson.dumps(result, ensure_ascii=True, sort_keys=True, indent=4))
+
+        info('\n\n results is written into %s' % result_json)
+
 
 def main():
-    Builder().build()
+    Builder().run()
 
 
 if __name__ == '__main__':
