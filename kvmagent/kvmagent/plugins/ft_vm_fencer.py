6ccc9cfbb245ed76fc5e9a050cf2ffd3dddc96d1
AlanJager <alanjager@outlook.com>
2021-04-22 09:53:52 +0800

    [BugFix: MINI-2784]
    Support virtual nics in bond
    
    Signed-off-by: AlanJager <alanjager@outlook.com>

--- kvmagent/kvmagent/plugins/ft_vm_fencer.py
+++ kvmagent/kvmagent/plugins/ft_vm_fencer.py
@@ -265,10 +265,15 @@ class FaultToleranceFecnerPlugin(kvmagent.KvmAgent):
             if is_bonding == 0:
                 return "up" in o
 
+            if getoutput("/bin/cat /sys/devices/virtual/net/%s/carrier" % device) == '1':
+                return True
+
+            if getoutput("/bin/cat /sys/class/net/%s/carrier" % device) == '1':
+                return True
+
             physical_nics = getoutput("find /sys/class/net -type l -not -lname '*virtual*' -printf '%f\\n'").splitlines()
             if True in [p.strip() == device for p in physical_nics]:
-                return getoutput("/sys/class/net/%s/carrier" % device) == 1
-
+                return getoutput("/bin/cat /sys/class/net/%s/carrier" % device) == '1'
             return None
 
         def do_test_network(target_ip):
