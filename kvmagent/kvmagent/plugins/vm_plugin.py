c7ad8bd6c1521426bee3104104229a8cf6527bd2
haibiao.xiao <haibiao.xiao@zstack.io>
2021-05-14 13:02:29 +0800

    fix bug: attach normal vxlan vpc network to vpc failed

--- kvmagent/kvmagent/plugins/vm_plugin.py
+++ kvmagent/kvmagent/plugins/vm_plugin.py
@@ -2726,7 +2726,7 @@ class Vm(object):
         brMode = cmd.addons['brMode'] if cmd.addons else None
         vhostSrcPath = None
 
-        if cmd.nic.type in "vDPA":
+        if cmd.nic.type == "vDPA":
             if action in "Attach":
                 vhostSrcPath = ovs.get_vDPA(cmd.vmUuid, cmd.nic)
             else:
@@ -2927,7 +2927,7 @@ class Vm(object):
                     # vf nic doesn't have internal name
                     if cmd.nic.pciDeviceAddress is not None:
                         return True
-                    if cmd.nic.type in "vDPA":
+                    if cmd.nic.type == "vDPA":
                         return True
                     else:
                         return linux.is_network_device_existing(cmd.nic.nicInternalName)
