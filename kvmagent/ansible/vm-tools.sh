0f484ba609ba32f8830655fd0779f608aeac2389
Zhang, Wenhao <wenhao.zhang@zstack.io>
2021-07-07 16:05:05 +0800

    [BugFix: ZSTAC-41487]
    Modify agent version file format
    
    We have modified the format of the agent version file to be compatible with the old version to ensure that the old version of guest tools can also be successfully upgraded automatically.

--- kvmagent/ansible/vm-tools.sh
+++ kvmagent/ansible/vm-tools.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-echo "installing zwatch-vm-agent"
-
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US:
 
-LABLE=''
-ARCH=`uname -m`
+SKIP_MD5_CHECK=""
+LOGPATH=/var/log/zstack
+LOGFLLE=zwatch-update.log
+LOGPATH_EXISTS=""
+
+while :
+do
+  [ -z "$1" ] && break;
+  case "$1" in
+    --skip-md5-check ) SKIP_MD5_CHECK='y'; shift;;
+    --log-path ) LOGPATH=$2; shift 2;;
+  esac
+done
 
 case `uname -s` in
   Linux)
@@ -18,50 +27,112 @@ case `uname -s` in
     ;;
 esac
 
-if [ x"$ARCH" != x"x86_64" ]; then
-    LABLE="_$ARCH"
+ARCH=`uname -m`
+if [ x"$ARCH" == x"x86_64" ]; then
+  LABLE=".${AGENT_OS}-amd64"
+else
+  LABLE=".${AGENT_OS}-${ARCH}"
 fi
-
 BIN_NAME="zwatch-vm-agent${LABLE}"
 
-AGENT_VERSION=`curl -s http://169.254.169.254/agent_version`
-if [[ ! ${AGENT_VERSION} =~ ${BIN_NAME} ]]; then
-    echo "can't get the ${BIN_NAME} latest version"
+if [ -d $LOGPATH ]; then
+  LOGPATH_EXISTS = "y"
+fi
+version=""
+
+# functions
+
+log_info() {
+  if [ x"$LOGPATH_EXISTS" == x"y" ]; then
+    echo "$(date) $1" >> "${LOGPATH}/${LOGFLLE}"
+  fi
+  if [ -z $2 ]; then
+    echo $1
+  fi
+}
+
+send_install_result() {
+  curl -H "Content-Type: application/json" -H "commandpath: /host/zwatchInstallResult" -X POST -d "{\"vmInstanceUuid\": \"${vmInstanceUuid}\", \"version\": \"$1\"}" http://169.254.169.254/host/zwatchInstallResult
+}
+
+check_version_file() {
+  AGENT_VERSION=`curl -s http://169.254.169.254/agent_version`
+  if [[ ! ${AGENT_VERSION} =~ ${BIN_NAME} ]]; then
+    log_info "Fail to parse agent_version file"
+    log_info "agent_version context: ${AGENT_VERSION}" not-print-to-screen
+    log_info "agent tools file name: ${BIN_NAME}" not-print-to-screen
     exit 1
-fi
+  fi
+}
 
-echo "downloading zwatch-vm-agent"
-curl http://169.254.169.254/zwatch-vm-agent --silent -O
+download_agent_tools() {
+  curl http://169.254.169.254/zwatch-vm-agent --silent -o zwatch-vm-agent.download
+}
 
-BIN_MD5=`md5sum zwatch-vm-agent | awk '{print $1}'`
-if [[ ! ${AGENT_VERSION} =~ "md5-${BIN_NAME}=${BIN_MD5}" ]]; then
-    echo "there was an error downloading the zwatch-vm-agent, check md5sum fail"
+clean_download_file() {
+  rm -f zwatch-vm-agent.download
+}
+
+check_md5() {
+  if [ x"$SKIP_MD5_CHECK" == x"y" ]; then
+    return
+  fi
+
+  BIN_MD5=`md5sum zwatch-vm-agent.download | awk '{print $1}'`
+  if [[ ! ${AGENT_VERSION} =~ "md5-${BIN_NAME}=${BIN_MD5}" ]]; then
+    log_info "there was an error downloading the zwatch-vm-agent, check md5sum fail"
     exit 1
-fi
+  fi
+}
 
-vmInstanceUuid=`curl http://169.254.169.254/2009-04-04/meta-data/instance-id`
-version=`echo $AGENT_VERSION | awk -F '=' '{print $2}' | awk '{print $1}'`
+query_agent_info() {
+  vmInstanceUuid=`curl http://169.254.169.254/2009-04-04/meta-data/instance-id`
+  version=`echo $AGENT_VERSION | grep "zwatch-vm-agent=" | awk -F '=' '{print $2}'`
+}
 
-echo "stopping zwatch-vm-agent"
-service zwatch-vm-agent stop
+stop_agent_tools() {
+  service zwatch-vm-agent stop
+}
 
-echo "install zwatch-vm-agent"
-chmod +x zwatch-vm-agent
-./zwatch-vm-agent -i
-if [ $? != 0 ]; then
-     curl -H "Content-Type: application/json" -H "commandpath: /host/zwatchInstallResult" -X POST -d "{\"vmInstanceUuid\": \"${vmInstanceUuid}\", \"version\": \"${InstallFailed}\"}" http://169.254.169.254/host/zwatchInstallResult
-     echo "install zwatch-vm-agent fail"
-     exit 1
-fi
+install_agent_tools() {
+  chmod +x zwatch-vm-agent.download
+  ./zwatch-vm-agent.download -i
+  clean_download_file
+  if [ $? != 0 ]; then
+    send_install_result "InstallFailed"
+    log_info "install zwatch-vm-agent fail"
+    exit 1
+  fi
+}
 
+start_agent_tools() {
+  service zwatch-vm-agent start
+  if [ $? != 0 ]; then
+    send_install_result "InstallFailed"
+    log_info "service zwatch-vm-agent start fail"
+    exit 1
+  fi
 
-echo "starting zwatch-vm-agent"
-service zwatch-vm-agent start
-if [ $? != 0 ]; then
-     curl -H "Content-Type: application/json" -H "commandpath: /host/zwatchInstallResult" -X POST -d "{\"vmInstanceUuid\": \"${vmInstanceUuid}\", \"version\": \"${InstallFailed}\"}" http://169.254.169.254/host/zwatchInstallResult
-     echo "service zwatch-vm-agent start fail"
-     exit 1
-fi
+  send_install_result $version
+}
 
-curl -H "Content-Type: application/json" -H "commandpath: /host/zwatchInstallResult" -X POST -d "{\"vmInstanceUuid\": \"${vmInstanceUuid}\", \"version\": \"${version}\"}" http://169.254.169.254/host/zwatchInstallResult
-echo "start zwatch-vm-agent successfully"
+# process
+
+log_info "installing zwatch-vm-agent"
+check_version_file
+
+log_info "downloading zwatch-vm-agent"
+download_agent_tools
+check_md5
+query_agent_info
+
+log_info "stopping zwatch-vm-agent"
+stop_agent_tools
+
+log_info "install zwatch-vm-agent"
+install_agent_tools
+
+log_info "starting zwatch-vm-agent"
+start_agent_tools
+
+log_info "start zwatch-vm-agent successfully"
