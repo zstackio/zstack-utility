f11ec65042241452005c44384cf886ff2e93fa36
Wei Wang <wei.wang@zstack.io>
2022-02-08 22:38:40 +0800

    <feature>[docs]: support antora
    New documentation strategy: doc within code!
    
    With doc within code, each commit which changed the logic should
    with documentaion change too.
    
    Documentaion should wrote by asciidoc which is a really good markup
    lanaguage for documentaion, it's readable by humans and easy to parse
    with powerful functions. It's also easy to build html webpages, pdf,
    epub or any other forms you love used to distribute.
    
    Apart from stanard asciidoc syntax, we also enabled Asciidoctor Kroki
    extension, so you can "write" almost all diagrams we want with
    maintainable diagram lanuage like PlantUML, GraphViz, Mermaid, Ditaa,
    etc.
    
    Rock with doc!
    
    Change-Id: I65646a68717a687a6c666c7a786761626b716468

--- zstackcli/zstackcli/cli.py
+++ zstackcli/zstackcli/cli.py
@@ -1008,7 +1008,7 @@ Parse command parameters error:
 
         self.read_more(num)
 
-    def show_help(self):
+    def show_help(self, all_params):
         help_string = text_doc.bold('Usage:')
         help_string += '''
 -------------------------------------------------------------------------------
