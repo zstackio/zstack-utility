f19959d90896e5c6682b6612f240a6bd2d2748fb
xingwei.yu <xingwei.yu@zstack.io>
2022-03-10 14:22:22 +0800

    <fix>[host]: Fix 4.3.25 HostOS failed to update the computing nodes in the cluster
    Resolves: ZSTAC-45482
    
    Change-Id: I7566696768727762786779647a78676870746864

--- kvmagent/ansible/kvm.py
+++ kvmagent/ansible/kvm.py
@@ -224,7 +224,7 @@ def install_kvm_pkg():
 
         aarch64_ns10 = "bridge-utils chrony conntrack-tools cyrus-sasl-md5 device-mapper-multipath expect ipmitool iproute ipset \
                         usbredir-server iputils iscsi-initiator-utils libvirt libvirt-client libvirt-python lighttpd lsof \
-                        net-tools nfs-utils nmap openssh-clients OpenIPMI pciutils pv rsync sed nettle \
+                        net-tools nfs-utils nmap openssh-clients OpenIPMI pciutils pv rsync sed nettle libselinux-devel \
                         smartmontools sshpass usbutils vconfig wget audit dnsmasq tar \
                         qemu collectd-virt storcli edk2-aarch64 python2-pyudev collectd-disk"
 
@@ -241,7 +241,7 @@ def install_kvm_pkg():
 
         x86_64_ns10 = "bridge-utils chrony conntrack-tools cyrus-sasl-md5 device-mapper-multipath expect ipmitool iproute ipset \
                         usbredir-server iputils iscsi-initiator-utils libvirt libvirt-client libvirt-python lighttpd lsof \
-                        net-tools nfs-utils nmap openssh-clients OpenIPMI pciutils pv rsync sed nettle \
+                        net-tools nfs-utils nmap openssh-clients OpenIPMI pciutils pv rsync sed nettle libselinux-devel \
                         smartmontools sshpass usbutils vconfig wget audit dnsmasq tar \
                         qemu collectd-virt storcli edk2-ovmf python2-pyudev collectd-disk"
 
