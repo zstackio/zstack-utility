3ce55b69c24ca3bb9e7ea47ed759d99a67ff5fa2
xiaoyingtao <2421815391@qq.com>
2022-03-23 14:37:34 +0800

    <fix>[host]: handle power_supply_out_power value is str
    Resolves: ZHCI-849
    
    Change-Id: I676d7a6a70646d646b776c6c6166726a76667668

--- kvmagent/kvmagent/plugins/prometheus.py
+++ kvmagent/kvmagent/plugins/prometheus.py
@@ -436,8 +436,8 @@ def collect_ipmi_state():
             ps_str = info.split("|")[0].strip().split(" ")[0].split("_")[1]
             if ps_str == 'POUT':
                 ps_out_power = info.split("|")[4].strip().lower()
-                ps_power_value = filter(str.isdigit, ps_out_power)
-                metrics['power_supply_current_output_power'].add_metric([ps_id], float(ps_power_value))
+                ps_out_power = float(filter(str.isdigit, ps_out_power)) if bool(re.search(r'\d', ps_out_power)) else info.split("|")[4].strip().lower()
+                metrics['power_supply_current_output_power'].add_metric([ps_id], ps_out_power)
             elif ps_str == 'Status':
                 ps_state = info.split("|")[4].strip().lower()
                 health = 0 if "presence detected" == ps_state else 10
