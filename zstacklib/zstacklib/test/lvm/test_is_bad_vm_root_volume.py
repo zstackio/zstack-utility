05a26391e286321ad5a11008d555e0ad55c97a5b
J M <jin.ma@zstack.io>
2022-05-12 16:20:23 +0800

    <fix>[ha]: enhance sblk fencer
    lv whose pv is missing should be bad volume.
    
    Resolves: ZSTAC-46216
    
    Change-Id: I7273626776687977627a727164756b6b73787274

--- zstacklib/zstacklib/test/lvm/test_is_bad_vm_root_volume.py
+++ zstacklib/zstacklib/test/lvm/test_is_bad_vm_root_volume.py
@@ -13,30 +13,80 @@ from zstacklib.utils import bash
 class Test(unittest.TestCase):
 
     def test_function_is_bad_vm_root_volume(self):
-
-        def mock_shell_output(path):
-            with open(path,'r') as r:
-                lines=r.read()
-            bash.bash_o = mock.Mock(return_value=lines)
-        
         def test_function_works_or_not(expect_is_bad_volume):
             is_bad_volume = lvm.is_bad_vm_root_volume("/dev/0af3ac2a9388489d8dcb0f3bf89f1af1/84c16257085b40de9823dc79d8de18f7")
             self.assertEqual(is_bad_volume, expect_is_bad_volume)
 
-        mock_shell_output("volume_not_exist.txt")
-        test_function_works_or_not(False)
+        lv_bad_output = [all_dep_missing_one_mpath_pv_lv, all_dep_offline_one_mpath_pv_lv, one_pv_blocked_one_pv_lv,
+                         some_dep_offline_one_mpath_pv_lv, some_dep_offline_two_mpath_pv_lv,
+                         one_pv_offline_two_mpath_pv_lv, one_pv_offline_two_pv_lv]
 
-        mock_shell_output("good_running_vloume.txt")
-        test_function_works_or_not(False)
+        lv_good_output = [all_dep_running_one_mpath_pv_lv]
 
-        mock_shell_output("bad_blocked_vloume.txt")
+        for o in lv_bad_output:
+            bash.bash_ro = mock.Mock(return_value=(0, o))
+            test_function_works_or_not(True)
+
+        for o in lv_good_output:
+            bash.bash_ro = mock.Mock(return_value=(0, o))
+            test_function_works_or_not(False)
+
+        bash.bash_ro = mock.Mock(return_value=(-1, ""))
         test_function_works_or_not(True)
 
-        mock_shell_output("bad_output.txt")
-        test_function_works_or_not(False)
 
-        mock_shell_output("bad_offline_multi_path_volume.txt")
-        test_function_works_or_not(True)
+volume_not_exist = ""
+
+all_dep_running_one_mpath_pv_lv = '''NAME TYPE STATE
+eff09a57d7854231b8200b7f83e3adb4-da3d8ec624ea412ebf1ea2bf54cd0d58 lvm running
+mpatha mpath running
+sda disk running
+sdb disk running'''
+
+some_dep_offline_one_mpath_pv_lv = '''NAME TYPE STATE
+eff09a57d7854231b8200b7f83e3adb4-da3d8ec624ea412ebf1ea2bf54cd0d58 lvm running
+mpatha mpath running
+sda disk transport-offline
+sdb disk running'''
+
+all_dep_offline_one_mpath_pv_lv = '''NAME TYPE STATE
+eff09a57d7854231b8200b7f83e3adb4-da3d8ec624ea412ebf1ea2bf54cd0d58 lvm running
+mpatha mpath running
+sda disk transport-offline
+sdb disk transport-offline'''
+
+some_dep_offline_two_mpath_pv_lv = '''NAME TYPE STATE
+cf1e9c4f3d674f159505c234c3e5356b-e7b20bbcad9f4e1499259e5b8ec0eccd lvm running
+36000d31000e568000000000000000103 mpath running
+sdaj disk running
+sdao disk running
+36000d31000e568000000000000000112 mpath running
+sdbg disk running
+sdax disk transport-offline'''
+
+one_pv_offline_two_mpath_pv_lv = '''NAME TYPE STATE
+cf1e9c4f3d674f159505c234c3e5356b-e7b20bbcad9f4e1499259e5b8ec0eccd lvm running
+36000d31000e568000000000000000103 mpath running
+sdaj disk running
+sdao disk running
+36000d31000e568000000000000000112 mpath running
+sdbg disk transport-offline
+sdax disk transport-offline'''
+
+
+one_pv_offline_two_pv_lv = '''NAME TYPE STATE
+cf1e9c4f3d674f159505c234c3e5356b-e7b20bbcad9f4e1499259e5b8ec0eccd lvm running
+sdaj disk transport-offline
+sdao disk running'''
+
+one_pv_blocked_one_pv_lv = '''NAME TYPE STATE
+cf1e9c4f3d674f159505c234c3e5356b-e7b20bbcad9f4e1499259e5b8ec0eccd lvm running
+sdaj disk blocked'''
+
+all_dep_missing_one_mpath_pv_lv = '''NAME TYPE STATE
+cf1e9c4f3d674f159505c234c3e5356b-e7b20bbcad9f4e1499259e5b8ec0eccd lvm running
+36000d31000e568000000000000000103 mpath running'''
+
 
 if __name__ == "__main__":
     unittest.main()
