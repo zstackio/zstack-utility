549b6a780d2a08c1e598d989d39cf7a17feff74c
J M <jin.ma@zstack.io>
2022-05-16 14:00:46 +0800

    <fix>[zstacklib]: enhance form error handle
    add try except to handle error. do not throw it.
    
    Resolves: ZSTAC-46216
    
    Change-Id: I736f7a63767767646773726b66776175666f7268

--- zstacklib/zstacklib/utils/form.py
+++ zstacklib/zstacklib/utils/form.py
@@ -1,6 +1,9 @@
+from zstacklib.utils import log
+
+logger = log.get_logger(__name__)
 
 
-def load(stdout, sep=None):
+def _load(stdout, sep=None):
     # type: (str, str) -> list[dict]
     ret = []
     lines = stdout.splitlines()
@@ -10,8 +13,20 @@ def load(stdout, sep=None):
     heads = lines[0].split(sep)
     for l in lines[1:]:
         o = {}
+        # init
+        for h in heads:
+            o[h] = None
+
         for i, v in enumerate(l.split(sep)):
             o[heads[i]] = v
         ret.append(o)
 
     return ret
+
+
+def load(stdout, sep=None):
+    try:
+        return _load(stdout, sep)
+    except Exception as e:
+        logger.debug("not a standard form:%s" % e.message)
+        return []
