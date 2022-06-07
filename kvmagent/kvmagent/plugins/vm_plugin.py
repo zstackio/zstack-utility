b9df7ad0007b1f412c54387d9ff254347f44dea4
Qun Li <qun.li@zstack.io>
2022-06-07 14:20:50 +0800

    <fix>[vm]: fix TypeError caused by NullPtr.
    Resolves: ZSTAC-47059
    
    Change-Id: I686961776b727a68786f6970756876657072677a
    
    Signed-off-by: Qun Li <qun.li@zstack.io>

--- kvmagent/kvmagent/plugins/vm_plugin.py
+++ kvmagent/kvmagent/plugins/vm_plugin.py
@@ -6608,6 +6608,8 @@ host side snapshot files chian:
 
             try:
                 volumes = isc.query_mirror_volumes(cmd.vmUuid)
+                if volumes is None:
+                    volumes = {}
                 target = volumes[device_name]
                 if target != cmd.mirrorTarget:
                     isc.stop_mirror(cmd.vmUuid, False, device_name)
