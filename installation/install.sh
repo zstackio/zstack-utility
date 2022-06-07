070146b2c72a1e11da66176b4a9f99aaeac7d3ae
zstackio <test.zstack@zstack.io>
2022-06-07 22:12:44 +0800

    Revert "Merge branch 'cherry-pick-5d39887f' into '4.2.25'"
    This reverts merge request !1789

--- installation/install.sh
+++ installation/install.sh
@@ -167,11 +167,6 @@ disable_zstack_tui() {
   systemctl restart getty@tty1.service
 }
 
-kill_zstack_tui_and_restart_tty1(){
-  pkill -9 zstack_tui
-  systemctl restart getty@tty1.service
-}
-
 # stop zstack_tui to prevent zstack auto installation
 stop_zstack_tui
 
@@ -3827,7 +3822,6 @@ if [ x"$UPGRADE" = x'y' ]; then
         echo ""
         echo_star_line
         disable_zstack_tui
-        kill_zstack_tui_and_restart_tty1
         exit 0
     fi
 
@@ -3873,7 +3867,6 @@ if [ x"$UPGRADE" = x'y' ]; then
     echo_custom_pcidevice_xml_warning_if_need
     echo_star_line
     disable_zstack_tui
-    kill_zstack_tui_and_restart_tty1
     post_scripts_to_restore_iptables_rules
     if [[ $DEBIAN_OS =~ $OS ]];then
         post_restore_source_on_debian
@@ -3901,7 +3894,6 @@ if [ ! -z $ONLY_INSTALL_LIBS ];then
     echo "P.S.: selinux is disabled!"
     echo_star_line
     disable_zstack_tui
-    kill_zstack_tui_and_restart_tty1
     exit 0
 fi
 
@@ -3929,7 +3921,6 @@ if [ ! -z $ONLY_INSTALL_ZSTACK ]; then
     check_ha_need_upgrade
     echo_star_line
     disable_zstack_tui
-    kill_zstack_tui_and_restart_tty1
     exit 0
 fi
 
@@ -4075,6 +4066,5 @@ echo_chrony_server_warning_if_need
 check_ha_need_upgrade
 echo_star_line
 disable_zstack_tui
-kill_zstack_tui_and_restart_tty1
 post_scripts_to_restore_iptables_rules
 
