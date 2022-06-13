8b4cac47131cb7fbd0b55717ac15722a43c6661d
ZqyDaoDao <249449726@qq.com>
2022-06-13 18:02:13 +0800

    <fix>[host]: display the vGPU types that can currently be created
    display the vGPU types that can currently be created
    
    Resolves: ZHCI-1009
    
    Change-Id: I6f72767170617268776a677371736c62636d6465

--- kvmagent/kvmagent/plugins/host_plugin.py
+++ kvmagent/kvmagent/plugins/host_plugin.py
@@ -1648,7 +1648,7 @@ done
             return False
 
         # check if nvidia vgpu is supported by current device
-        r, o, e = bash_roe("nvidia-smi vgpu -i %s -v -s" % addr)
+        r, o, e = bash_roe("nvidia-smi vgpu -i %s -v -c" % addr)
         if r != 0:
             return False
 
