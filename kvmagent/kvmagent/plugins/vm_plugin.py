b1953e3e02a2e2f17b22d9596a9752f934f9f3b8
Wenhao, Zhang <wenhao.zhang@zstack.io>
2022-06-15 14:54:58 +0800

    <feature>[mevoco]: add sync VM clock handler
    Relected: ZSTAC-46810
    
    Change-Id: I6f676a64686e7275726372637576746a66637863

--- kvmagent/kvmagent/plugins/vm_plugin.py
+++ kvmagent/kvmagent/plugins/vm_plugin.py
@@ -5017,6 +5017,8 @@ class VmPlugin(kvmagent.KvmAgent):
     FAIL_COLO_PVM_PATH = "/fail/colo/pvm"
     GET_VM_DEVICE_ADDRESS_PATH = "/vm/getdeviceaddress"
     SET_EMULATOR_PINNING_PATH = "/vm/emulatorpinning"
+    SYNC_VM_CLOCK_PATH = "/vm/clock/sync"
+    SET_SYNC_VM_CLOCK_TASK_PATH = "/vm/clock/sync/task"
 
     VM_CONSOLE_LOGROTATE_PATH = "/etc/logrotate.d/vm-console-log"
 
@@ -7755,6 +7757,24 @@ host side snapshot files chian:
             shell.call('%s %s' % (cmd_base, cmd.emulatorPinning))
         return jsonobject.dumps(rsp)
 
+    def sync_vm_clock_now(self, req):
+        cmd = jsonobject.loads(req[http.REQUEST_BODY])
+        rsp = kvmagent.AgentResponse()
+
+        vm = get_vm_by_uuid_no_retry(cmd.vmUuid, True)
+        if vm.state != Vm.VM_STATE_RUNNING:
+            rsp.success = False
+            rsp.error = 'vm[uuid:%s, name:%s] is not in running state' % (cmd.vmUuid, vm.get_name())
+            return jsonobject.dumps(rsp)
+
+        vm._wait_until_qemuga_ready(3000, cmd.vmUuid)
+        script = '''virsh qemu-agent-command %s --cmd "{\\"execute\\":\\"guest-set-time\\",\\"arguments\\":{\\"time\\":`date +%%s%%N`}}"''' % cmd.vmUuid
+        r, o, e = bash.bash_roe(script)
+        if r != 0:
+            rsp.success = False
+            rsp.error = "failed to sync time for vm[uuid:%s]: %s, %s" % (cmd.vmUuid, o, e)
+        return jsonobject.dumps(rsp)
+
     @staticmethod
     def _find_volume_device_address(vm, volumes):
         #  type:(Vm, list[jsonobject.JsonObject]) -> list[VmDeviceAddress]
@@ -7865,6 +7885,7 @@ host side snapshot files chian:
         http_server.register_async_uri(self.FAIL_COLO_PVM_PATH, self.fail_colo_pvm, cmd=FailColoPrimaryVmCmd())
         http_server.register_async_uri(self.GET_VM_DEVICE_ADDRESS_PATH, self.get_vm_device_address)
         http_server.register_async_uri(self.SET_EMULATOR_PINNING_PATH, self.set_emulator_pinning)
+        http_server.register_async_uri(self.SYNC_VM_CLOCK_PATH, self.sync_vm_clock_now)
 
         self.clean_old_sshfs_mount_points()
         self.register_libvirt_event()
