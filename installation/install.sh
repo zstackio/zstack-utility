f30374a2bc5fc6e928b2e8bd84dea5450bd56343
boce.wang <boce.wang@zstack.io>
2022-09-22 10:11:46 +0800

    <feature>[vpc]: ipsec optimizing and status monitor
    detect vpc ipsec status and optimizing
    
    Resolves: ZSTAC-48886
    
    Change-Id:I6868776ce1c816eab42c4ada9f349c83530fd33b

--- installation/install.sh
+++ installation/install.sh
@@ -208,6 +208,7 @@ declare -a upgrade_params_array=(
     '4.4.0,-Dlicense.upgradeLicenseRecords=true'
     '4.4.30,-DupgradeVpcHaL3NetworkCheck=true'
     '4.5.0,-DupgradeVipOwner=true'
+    '4.5.0,-DupgradeVpcIpsecVersionCheck=true'
 )
 #other than the upon params_array, this one could be persisted in zstack.properties
 declare -a upgrade_persist_params_array=(
