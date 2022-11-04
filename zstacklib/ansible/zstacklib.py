2ec6262dbde5a8f3588247a5781e1b173a9bcc28
junfei.wang <junfei.wang@zstack.io>
2022-11-04 16:54:37 +0800

    <fix>[zstacklib]:support redhat os
    add a config in RPM_BASED_OS
    resolves:ZSTAC-49220
    Change-Id: I61786e7a6a786c7361767861736b726562708599

--- zstacklib/ansible/zstacklib.py
+++ zstacklib/ansible/zstacklib.py
@@ -30,7 +30,7 @@ ansible.constants.HOST_KEY_CHECKING = False
 enable_networkmanager_list = ["ns10", "euler20", "uos1021a", "nfs4"]
 supported_arch_list = ["x86_64", "aarch64", "mips64el", "loongarch64"]
 
-RPM_BASED_OS = ["kylin_zstack", "kylin_tercel", "kylin_sword", "alibaba", "centos", "openeuler", "uniontech_kongzi", "nfs"]
+RPM_BASED_OS = ["kylin_zstack", "kylin_tercel", "kylin_sword", "alibaba", "centos", "openeuler", "uniontech_kongzi", "nfs", "redhat"]
 DEB_BASED_OS = ["ubuntu", "uos", "kylin4.0.2", "debian", "uniontech_fou"]
 DISTRO_WITH_RPM_DEB = ["kylin", "uniontech"]
 
