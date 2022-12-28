1efdd014773760ea86f485c2e411ff686f6c59db
lining <2016220101025@std.uestc.edu.cn>
2022-12-28 12:50:35 +0800

    <fix>[installation]: skip prepare zops user under aarch64 architecture
    Resolves: ZSTAC-51932
    
    Change-Id: I71786f756c7269626a767662616464676f696476

--- installation/install.sh
+++ installation/install.sh
@@ -3302,6 +3302,17 @@ prepare_zops_user_and_db() {
     MYSQL_DATA_DIR=`cat $MYSQL_CONF_FILE | grep datadir | cut -d '=' -f 2`
     # check zops db is already, if have been created, do nothing
     [ -f $MYSQL_DATA_DIR/$db/db.opt ] && return 0
+
+    # zops does not support aarch64 architecture, skip it
+    if [ $BASEARCH = "aarch64" ]; then
+        return 0
+    fi
+
+    # zops does not support kylin os, skip it
+    if [ -f /etc/kylin-release ]; then
+        return 0
+    fi
+
     mysql -u root --password=$MYSQL_NEW_ROOT_PASSWORD -e 'exit' >/dev/null 2>&1
     # check mysql root password not changed, if not changed, do nothing
     [ $? -eq 0 ] && return 0
