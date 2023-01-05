5a5eca9effc45286d9ccdd463bf942f73260f7e7
J M <jin.ma@zstack.io>
2023-01-06 02:38:41 +0800

    <fix>[kvmagent]: correct regex in multipath.conf
    * is not a standard regular expression, use .*
    
    Resolves: ZSTAC-49260
    
    Change-Id: I637968646d6f73666364737972666f766d6f746c

--- kvmagent/kvmagent/plugins/storage_device.py
+++ kvmagent/kvmagent/plugins/storage_device.py
@@ -2,6 +2,9 @@ import random
 import time
 import string
 import os.path
+
+import simplejson
+
 from kvmagent import kvmagent
 from kvmagent.plugins import vm_plugin
 from zstacklib.utils import multipath
@@ -940,7 +943,7 @@ class StorageDevicePlugin(kvmagent.KvmAgent):
     @bash.in_bash
     def enable_multipath(self, req):
         rsp = AgentRsp()
-        cmd = jsonobject.loads(req[http.REQUEST_BODY])
+        cmd_dict = simplejson.loads(req[http.REQUEST_BODY])
         lvm.enable_multipath()
 
         r = bash.bash_r("grep '^[[:space:]]*alias' /etc/multipath.conf")
@@ -948,7 +951,7 @@ class StorageDevicePlugin(kvmagent.KvmAgent):
             bash.bash_roe("sed -i 's/^[[:space:]]*alias/#alias/g' /etc/multipath.conf")
             bash.bash_roe("systemctl reload multipathd")
 
-        if multipath.write_multipath_conf("/etc/multipath.conf", cmd.blacklist):
+        if multipath.write_multipath_conf("/etc/multipath.conf", cmd_dict.get("blacklist", None)):
             bash.bash_roe("systemctl reload multipathd")
 
         linux.set_fail_if_no_path()
@@ -960,8 +963,9 @@ class StorageDevicePlugin(kvmagent.KvmAgent):
         lvm.disable_multipath()
         return jsonobject.dumps(rsp)
 
-    def get_device_info(self, scsi_info, rescan):
-        # type: (str) -> FiberChannelLunStruct
+    @staticmethod
+    def get_device_info(scsi_info, rescan):
+        # type: (str, bool) -> FiberChannelLunStruct
         s = FiberChannelLunStruct()
 
         dev_name = os.path.basename(scsi_info.split()[-2])
--- zstacklib/zstacklib/utils/multipath.py
+++ zstacklib/zstacklib/utils/multipath.py
@@ -7,6 +7,8 @@ MULTIPATH_PATH = "/etc/multipath.conf"
 
 
 def parse_multipath_conf(conf_lines):
+    # type: (iter) -> list[dict[str, list]]
+
     config = []
     for line in conf_lines:
         line = line.rstrip().strip()
@@ -20,86 +22,92 @@ def parse_multipath_conf(conf_lines):
             else:
                 line = line.split()
                 if len(line) > 1:
-                    config.append({line[0]: " ".join(line[1:])})
+                    config.append({line[0]: (" ".join(line[1:])).strip('"')})
     return config
 
 
-def write_multipath_conf(path, multipath_blacklist=None):
-    device = {'device': [{'features': '"0"'}, {'no_path_retry': '"fail"'}, {'product': '"*"'}, {'vendor': '"*"'}]}
-    devices = {
-        'devices': [{'device': [{'vendor': '*'}, {'product': '*'}, {'features': '0'}, {'no_path_retry': 'fail'}]}]}
-    feature = 'queue_if_no_path'
-    skipWrite = False
-    deleteFeature = False
-    blacklist = parse_blacklist(multipath_blacklist)
-    update_blacklist = blacklist is not None
+def sorted_conf(sections):
+    # type: (list) -> list
+
+    result = []
+    if not sections:
+        return result
+
+    for section in sorted(sections, key=lambda s: s.keys()[0]):
+        section_name, section_value = section.items()[0]
+        if type(section_value) is list:
+            result.append({section_name: sorted_conf(section_value)})
+        else:
+            result.append({section_name: section_value})
+
+    return result
+
+
+def write_multipath_conf(path, blacklist=None):
+    # type: (str, list[dict[str, object]]) -> bool
+
+    default_device = {'device': [{'features': '0'}, {'no_path_retry': 'fail'}, {'product': '.*'}, {'vendor': '.*'}]}
+    feature_to_remove = 'queue_if_no_path'
+    modified = False
     with open(path, 'r+') as fd:
         config = parse_multipath_conf(fd)
-        isAddDevices = True
-        for item in config:
-            if 'devices' in item:
-                for devices_k, devices_v in item.items():
-                    for device_dict in devices_v:
-                        for device_k, device_v in device_dict.items():
-                            for device_feature in device_v:
-                                if feature in str(device_feature):
-                                    device_v.remove(device_feature)
-                                    deleteFeature = True
-                                    print config
+        has_devices_section = False
+        has_default_device = False
+        blacklist_changed = False
+        for section in config:
+            if 'blacklist' in section:
+                blacklist_changed = cmp(sorted_conf(section['blacklist']), sorted_conf(blacklist)) != 0
 
-                        if 'device' in device_dict and cmp(sorted(device['device']), sorted(device_dict['device'])) == 0:
-                            skipWrite = True
+            if 'devices' in section:
+                has_devices_section = True
+                for subsection in section['devices']:
+                    for attribute in subsection['device'][:]:
+                        name, value = attribute.items()[0]
+                        if value.strip().strip('"') == '*':
+                            attribute[name] = '.*'
+                            modified = True
 
-                isAddDevices = False
+                        if name == 'features' and feature_to_remove in value:
+                            subsection['device'].remove(attribute)
+                            modified = True
 
-                if skipWrite is False:
-                    item['devices'].append(device)
+                        if cmp(sorted(default_device['device']), sorted(subsection['device'])) == 0:
+                            has_default_device = True
 
-            if 'blacklist' in item and update_blacklist and cmp(item['blacklist'], blacklist) == 0:
-                update_blacklist = False
+                if not has_default_device:
+                    section['devices'].append(default_device)
+                    modified = True
 
-        if update_blacklist:
+        if blacklist is not None and blacklist_changed:  # None blacklist means ignore
             config = filter(lambda cfg : 'blacklist' not in cfg, config)
             config.append({'blacklist' : blacklist})
+            modified = True
 
-        if isAddDevices is True:
-            config.append(devices)
+        if not has_devices_section:
+            config.append({'devices': [default_device]})
+            modified = True
+
         logger.info(config)
-        if skipWrite is False or deleteFeature is True or update_blacklist is True:
+        if modified:
             fd.seek(0)
             fd.truncate()
 
-            for parent_dict in config:
-                for parent_k, parent_v in parent_dict.items():
-                    fd.write(parent_k + " ")
-                    fd.write("{\n")
-                    for child_dict in parent_v:
-                        if type(child_dict.values()[0]) == str:
-                            fd.write('    %s "%s"\n' % (child_dict.keys()[0].replace('"', ''), child_dict.values()[0].replace('"', '')))
-                            continue
-                        for child_k, child_v in child_dict.items():
-                            fd.write('    %s {\n' % child_k)
-                            for leaf_dict in child_v:
-                                fd.write('        %s "%s"\n' % (leaf_dict.keys()[0].replace('"', ''), leaf_dict.values()[0].replace('"', '')))
-                            fd.write("    }\n")
+            for section in config:
+                section_name, section_value = section.items()[0]
+                fd.write("%s {\n" % section_name)
+                for child in sorted_conf(section_value):
+                    child_name, child_value = child.items()[0]
+                    # child is attribute
+                    if type(child_value) == str:
+                        fd.write('\t%s "%s"\n' % (child_name.strip('"'), child_value.strip('"')))
+                        continue
 
-                    fd.write("\n}\n")
+                    # child is subsection
+                    fd.write('\t%s {\n' % child_name)
+                    for attribute in child_value:
+                        attrib_name, attrib_value = attribute.items()[0]
+                        fd.write('\t\t%s "%s"\n' % (attrib_name.strip('"'), attrib_value.strip('"')))
+                    fd.write("\t}\n")
+                fd.write("}\n")
 
-    return not skipWrite or deleteFeature or update_blacklist
-
-
-def parse_blacklist(blacklist):
-    if blacklist is None:
-        return
-
-    return_list = []
-    for item_obj in blacklist:
-        item_dict = item_obj.__dict__
-        if 'device' in item_dict:
-            sub_item_list = []
-            for sub_item_obj in item_dict['device']:
-                sub_item_list.append(sub_item_obj.__dict__)
-            item_dict['device'] = sub_item_list
-        return_list.append(item_dict)
-
-    return return_list
+    return modified
