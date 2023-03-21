ff40e7b168ec2d702cfe8db85f7b45b1fd961432
hanyu.liang <hanyu.liang@zstack.io>
2023-03-21 17:15:30 +0800

    <fix>[install]: fix mn installation process is refreshed
    Resolves: ZSTAC-53509
    
    Change-Id: I7174535093214677a6b74646a786c7062696b62

--- installation/install.sh
+++ installation/install.sh
@@ -168,9 +168,8 @@ disable_zstack_tui() {
   systemctl daemon-reload
 }
 
-kill_zstack_tui_and_clear(){
+kill_zstack_tui(){
   pkill -9 zstack_tui
-  clear
 }
 
 # stop zstack_tui to prevent zstack auto installation
@@ -4094,7 +4093,7 @@ if [ x"$UPGRADE" = x'y' ]; then
         echo ""
         echo_star_line
         disable_zstack_tui
-        kill_zstack_tui_and_clear
+        kill_zstack_tui
         exit 0
     fi
 
@@ -4140,7 +4139,7 @@ if [ x"$UPGRADE" = x'y' ]; then
     echo_custom_pcidevice_xml_warning_if_need
     echo_star_line
     disable_zstack_tui
-    kill_zstack_tui_and_clear
+    kill_zstack_tui
     post_scripts_to_restore_iptables_rules
     if [[ $DEBIAN_OS =~ $OS ]];then
         post_restore_source_on_debian
@@ -4168,7 +4167,7 @@ if [ ! -z $ONLY_INSTALL_LIBS ];then
     echo "P.S.: selinux is disabled!"
     echo_star_line
     disable_zstack_tui
-    kill_zstack_tui_and_clear
+    kill_zstack_tui
     exit 0
 fi
 
@@ -4196,7 +4195,7 @@ if [ ! -z $ONLY_INSTALL_ZSTACK ]; then
     check_ha_need_upgrade
     echo_star_line
     disable_zstack_tui
-    kill_zstack_tui_and_clear
+    kill_zstack_tui
     exit 0
 fi
 
@@ -4359,6 +4358,6 @@ echo_chrony_server_warning_if_need
 check_ha_need_upgrade
 echo_star_line
 disable_zstack_tui
-kill_zstack_tui_and_clear
+kill_zstack_tui
 post_scripts_to_restore_iptables_rules
 
