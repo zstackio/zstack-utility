35e39f26605627aeb568c57df3dd774f7ef33545
hanyu.liang <hanyu.liang@zstack.io>
2023-05-22 17:24:05 +0800

    <fix>[install]: Fix the log is overwritten after each mn upgrade
    Resolves: ZSTAC-50244
    
    Change-Id: I61627750244344681234766678617771706e7878

--- installation/install.sh
+++ installation/install.sh
@@ -47,9 +47,9 @@ CUBE_INSTALL='n'
 SANYUAN_INSTALL='n'
 SDS_INSTALL='n'
 MANAGEMENT_INTERFACE=`ip route | grep default | head -n 1 | cut -d ' ' -f 5`
-ZSTACK_INSTALL_LOG='/tmp/zstack_installation.log'
-ZSTACKCTL_INSTALL_LOG='/tmp/zstack_ctl_installation.log'
-ZSTACK_TIMESTAMP_LOG='/tmp/zstack_installation_timestamp.log'
+ZSTACK_INSTALL_LOG="/tmp/zstack_installation-$(date +%Y-%m-%d-%H:%M:%S).log"
+ZSTACKCTL_INSTALL_LOG="/tmp/zstack_ctl_installation-$(date +%Y-%m-%d-%H:%M:%S).log"
+ZSTACK_TIMESTAMP_LOG="/tmp/zstack_installation_timestamp-$(date +%Y-%m-%d-%H:%M:%S).log"
 echo -e "Start the installation at "$(date +%Y-%m-%d' '%H:%M:%S,$((`date +10#%N`/1000000))) | tee -a $ZSTACK_INSTALL_LOG 1>/dev/null
 INSTALLATION_FAILURE=/tmp/zstack_installation_failure_exit_code
 [ -f $INSTALLATION_FAILURE ] && /bin/rm -f $INSTALLATION_FAILURE
