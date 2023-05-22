0139395b8463ddfa8d218194d654d0867a7aa13f
kaicai.hu <kaicai.hu@zstack.io>
2023-05-22 16:33:13 +0800

    <fix>[zstacklib]: fix collect hardware status failed when cube upgrade
    Resolves: ZHCI-1827
    
    Change-Id: I78666165647569636b6f676c6e667a6e786f6d76

--- zstackctl/zstackctl/ctl.py
+++ zstackctl/zstackctl/ctl.py
@@ -6468,11 +6468,18 @@ class CollectLogCmd(Command):
 
 
 def is_hyper_converged_host():
-    r, o = commands.getstatusoutput("bootstrap is_deployed")
-    if r != 0 or o.strip() != "true":
+    if not os.path.exists("/usr/local/hyperconverged"):
         return False
-    return True
-
+    
+    # Compatible with lower versions
+    if os.path.exists("/usr/local/hyperconverged/conf/host_info.json"):
+        return True
+    
+    if os.path.exists("/usr/local/hyperconverged/conf/deployed_info"):
+        return True
+    
+    return False
+    
 
 def get_hci_detail_version():
     detailed_version_file = "/usr/local/hyperconverged/conf/VERSION"
