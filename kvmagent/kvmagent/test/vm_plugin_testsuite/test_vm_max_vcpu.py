68a4ee53fd723b0e816fd0402ae8c785b429dea7
AlanJager <ye.zou@zstack.io>
2023-05-25 11:59:14 +0800

    <fix>[kvmagent]: test maximum vcpu on x86 can be set to 240
    Resolves: ZSTAC-45100
    
    Change-Id: I676c696e6f6f7766626273626d6362747673736d
    Signed-off-by: AlanJager <ye.zou@zstack.io>
    (cherry picked from commit c80daa0b1e9b837e756ab6975443db7f60968d0e)

--- kvmagent/kvmagent/test/vm_plugin_testsuite/test_vm_max_vcpu.py
+++ kvmagent/kvmagent/test/vm_plugin_testsuite/test_vm_max_vcpu.py
@@ -18,6 +18,23 @@ class TestVmMaxVcpu(TestCase, vm_utils.VmPluginTestStub):
         network_utils.create_default_bridge_if_not_exist()
 
     @pytest_utils.ztest_decorater
+    def test_set_max_vcpu_to_240(self):
+        vm = vm_utils.create_startvm_body_jsonobject()
+        vm.maxVcpuNum = 240
+        vm.socketNum = 240
+        vm.cpuOnSocket = 1
+        vm.threadsPerCore = 1
+        vm.cpuNum = 1
+        vm_utils.create_vm(vm)
+
+        _, o = bash.bash_ro(
+            "virsh dumpxml %s | grep vcpu" % vm.vmInstanceUuid)
+        self.assertEqual(o.strip(), "<vcpu placement='static' current='1'>240</vcpu>",
+                            "vcpu not configured as expected %s" % o)
+
+        self._destroy_vm(vm.vmInstanceUuid)
+
+    @pytest_utils.ztest_decorater
     def test_missing_max_vcpu(self):
         vm = vm_utils.create_startvm_body_jsonobject()
         vm.maxVcpuNum = None
