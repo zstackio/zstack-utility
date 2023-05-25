c9943e1e1e652ad3d1a952b417a37bbcbade155e
littleya <me@littleya.com>
2023-05-25 14:15:51 +0800

    <fix>[zstacklib]: Support upgrade from c76/c79 to h76c/h79c
    Support upgrade from c76/c79 to h76c/h79c, there are two scenarios:
    1. mn is h76c/h79c, host needs to upgrade from c76/c79 to h76c/h79c
    2. mn is other distro, but hash h76c/h79c repo, and host is c76/c79,
       need to upgrade to h76c/h79c
    
    The upgrade process is as follow:
    1. copy hexli-release to host and install it
    2. copy zstack-release to host and install it
    
    Resolves: ZSTAC-55827
    
    Change-Id: I69757a6c6962726166626a66616f676c7a796366

--- kvmagent/ansible/kvm.py
+++ kvmagent/ansible/kvm.py
@@ -80,6 +80,7 @@ if remote_pass is not None and remote_user != 'root':
     host_post_info.become = True
 
 host_info = get_remote_host_info_obj(host_post_info)
+host_info = upgrade_to_helix(host_info, host_post_info)
 releasever = get_host_releasever(host_info)
 host_post_info.releasever = releasever
 
