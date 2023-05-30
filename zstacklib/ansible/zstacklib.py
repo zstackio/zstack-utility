4d86215630f2fb26042608d17e3737611b6ffafe
littleya <me@littleya.com>
2023-05-30 12:14:49 +0800

    <fix>[zstacklib]: Improve ansible execution efficiency
    Improve ansible execution efficiency by caching inventory,
    variable_manager.
    
    Resolves: ZSTAC-56230
    
    Change-Id: I6b72646679716173676c6964727a6a68636a7961

--- zstacklib/ansible/zstacklib.py
+++ zstacklib/ansible/zstacklib.py
@@ -8,6 +8,7 @@ from logging import handlers as logging_handlers
 import json
 import os
 import sys
+import threading
 
 from ansible import constants as ansible_constants
 from ansible import context as ansible_context
@@ -103,6 +104,103 @@ class MemCache(cache.BaseCacheModule):
 
 
 memory.CacheModule = MemCache
+
+
+vm_cache = {}
+
+class VM(ansible_vm.VariableManager):
+    def __init__(self, loader, inventory, key):
+        super(VM, self).__init__(loader, inventory)
+        self._key = key
+
+    def get_vars(self, *args, **kwargs):
+        global vm_cache
+        flag = str(args) + str(kwargs) + self._key
+        if flag not in vm_cache:
+            vars = super(VM, self).get_vars(*args, **kwargs)
+            vm_cache[flag] = vars
+        return vm_cache[flag]
+
+
+ansible_module_cache = {}
+
+
+class AnsibleModules(object):
+    def __init__(self, runner_args):
+        self.host_inventory = runner_args.host_post_info.host_inventory
+        self.private_key = runner_args.host_post_info.private_key
+        self.host = runner_args.host_post_info.host
+        self.module_name = runner_args.module_name
+        self.module_args = runner_args.module_args
+        self.remote_port = runner_args.host_post_info.remote_port
+        self.remote_user = runner_args.host_post_info.remote_user
+        self.remote_pass = runner_args.host_post_info.remote_pass
+        self.become = runner_args.host_post_info.become
+        self.become_user = runner_args.host_post_info.become_user
+        self.become_pass = runner_args.host_post_info.remote_pass
+        self.transport = runner_args.host_post_info.transport
+        self.environment = runner_args.host_post_info.environment
+
+        global ansible_module_cache
+        self.key = '%s-%s' % (self.host, threading.currentThread().ident)
+        if self.key not in ansible_module_cache:
+            ansible_module_cache[self.key] = {}
+
+    @property
+    def _passwords(self):
+        return {
+            'conn_pass': self.remote_pass,
+            'become_pass': self.become_pass
+        }
+
+    @property
+    def loader(self):
+        if not hasattr(self, 'data_loader'):
+            self.data_loader = ansible_dataloader.DataLoader()
+        return self.data_loader
+
+    @property
+    def result_callback(self):
+        if not hasattr(self, 'rc'):
+            self.rc = ResultsCollectorJSONCallback()
+        return self.rc
+
+    @property
+    def inventory(self):
+        global ansible_module_cache
+        if'inventory' not in ansible_module_cache[self.key]:
+            inv = ansible_im.InventoryManager(
+                loader=self.loader,
+                sources=self.host_inventory)
+            ansible_module_cache[self.key]['inventory'] = inv
+        if not hasattr(self, 'inv'):
+            self.inv = ansible_module_cache[self.key]['inventory']
+        return self.inv
+
+    @property
+    def variable_manager(self):
+        global ansible_module_cache
+        if'variable_manager' not in ansible_module_cache[self.key]:
+            vm = VM(loader=self.inventory._loader,
+                    inventory=self.inventory,
+                    key=self.key)
+            ansible_module_cache[self.key]['variable_manager'] = vm
+        if not hasattr(self, 'vm'):
+            self.vm = ansible_module_cache[self.key]['variable_manager']
+        return self.vm
+
+    @property
+    def task_queue_manager(self):
+        if not hasattr(self, 'tqm'):
+            self.tqm = ansible_tqm.TaskQueueManager(
+                inventory=self.inventory,
+                variable_manager=self.variable_manager,
+                loader=self.inventory._loader,
+                passwords=self._passwords,
+                stdout_callback=self.result_callback,
+                run_additional_callbacks=False)
+            self.tqm._callbacks_loaded = True
+        return self.tqm
 
 
 class AgentInstallArg(object):
@@ -283,25 +381,7 @@ class ZstackRunner(object):
             become_method='sudo',
             ssh_extra_args='-C -o ControlMaster=auto -o ControlPersist=1800s',
         )
-        self.loader = ansible_dataloader.DataLoader()
-        self.result_callback = ResultsCollectorJSONCallback()
-
-        passwords = {
-            'conn_pass': self.remote_pass,
-            'become_pass': self.become_pass
-        }
-        self.inventory = ansible_im.InventoryManager(
-            loader=self.loader,
-            sources=self.host_inventory)
-        self.variable_manager = ansible_vm.VariableManager(
-            loader=self.loader,
-            inventory=self.inventory)
-        self.tqm = ansible_tqm.TaskQueueManager(
-            inventory=self.inventory,
-            variable_manager=self.variable_manager,
-            loader=self.loader,
-            passwords=passwords,
-            stdout_callback=self.result_callback)
+        self.ansible_module = AnsibleModules(runner_args)
 
     def run(self):
         action = {'module': self.module_name}
@@ -321,15 +401,15 @@ class ZstackRunner(object):
         )
         play = ansible_play.Play().load(
             play_source,
-            variable_manager=self.variable_manager,
-            loader=self.loader)
+            variable_manager=self.ansible_module.variable_manager,
+            loader=self.ansible_module.loader)
         try:
-            ret = self.tqm.run(play)
+            ret = self.ansible_module.task_queue_manager.run(play)
         finally:
-            self.tqm.cleanup()
-            if self.loader:
-                self.loader.cleanup_all_tmp_files()
-        return self.result_callback.fetch_result()
+            self.ansible_module.task_queue_manager.cleanup()
+            if self.ansible_module.tqm._loader:
+                self.ansible_module.tqm._loader.cleanup_all_tmp_files()
+        return self.ansible_module.result_callback.fetch_result()
 
 
 def banner(text, ch='*', length=78):
