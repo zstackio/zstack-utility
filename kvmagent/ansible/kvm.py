454847d697842726f591b895670dcea8eb77d276
yu.sun <yu.sun@zstack.io>
2023-08-02 10:11:56 +0800

    <fix>[ansible]: ZStack upgrade does not upgrade libvirt
    Resolves: ZSTAC-58121
    
    Change-Id: I6d69777475726a67796e69716263706c6a616766

--- kvmagent/ansible/kvm.py
+++ kvmagent/ansible/kvm.py
@@ -267,10 +267,8 @@ def install_kvm_pkg():
             host_post_info.post_label = "ansible.shell.install.pkg"
             host_post_info.post_label_param = "libvirt"
             (status, output) = run_remote_command(command, host_post_info, True, True)
-
-            versions = host_info.distro_version.split('.')
-            if output and len(versions) > 2 and versions[0] == '7' and versions[1] == '2':
-                dep_list = dep_list.replace('libvirt libvirt-client libvirt-python ', '')
+            if output:
+                dep_list =' '.join([pkg for pkg in dep_list.split() if not pkg.startswith('libvirt')]) 
 
             # add extra package
             if extra_packages != '':
