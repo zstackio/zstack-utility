c54278e2214f516e67e7eb663e315ec346395b74
Lei Liu <lei.liu@zstack.io>
2023-08-25 13:14:27 +0800

    <fix>[vm]: change special device slot
    Change-Id: I64616c776e7a65796166726a6d6a726c73616372

--- kvmagent/kvmagent/plugins/vm_plugin.py
+++ kvmagent/kvmagent/plugins/vm_plugin.py
@@ -3815,9 +3815,9 @@ class Vm(object):
             vendor_id, model_name = linux.get_cpu_model()
             if bash.bash_r('getenforce | grep -i dis') != 0:
                 e(qcmd, "qemu:arg", attrib={"value": "--device"})
-                e(qcmd, "qemu:arg", attrib={"value": "lkmc_pci,addr=05.0"})
+                e(qcmd, "qemu:arg", attrib={"value": "lkmc_pci,addr=1e.0"})
                 e(qcmd, "qemu:arg", attrib={"value": "--device"})
-                e(qcmd, "qemu:arg", attrib={"value": "vboot,addr=04.0"})
+                e(qcmd, "qemu:arg", attrib={"value": "vboot,addr=1f.0"})
 
             if "hygon" in model_name.lower() and cmd.hygonCpu:
                 e(qcmd, "qemu:arg", attrib={"value": "-cpu"})
