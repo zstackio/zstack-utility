8ff30b592959c5ee17b8d9f4a702aa0e0e10e5b9
shan.wu <shan.wu@zstack.io>
2023-12-07 15:51:03 +0800

    <fix>[storage-device]: get lun multipath info
    support obtaining multipath information for lun, including: path、status、name、target
    
    Resolves: ZSV-3316
    
    Change-Id:F556895F7175404EA8B056198ADED384

--- kvmagent/kvmagent/plugins/storage_device.py
+++ kvmagent/kvmagent/plugins/storage_device.py
@@ -170,6 +170,15 @@ class FcSanScanRsp(AgentRsp):
         self.fiberChannelLunStructs = []
         self.hbaWwnns = []
 
+class GetMultipathTopologyRsp(AgentRsp):
+    def __init__(self):
+        super(GetMultipathTopologyRsp, self).__init__()
+        self.devices = {} # type: dict[str, Device]
+
+class Device(object):
+    def __init__(self, disk, status):
+        self.disk = disk
+        self.state = status
 
 class NvmeSanScanRsp(AgentRsp):
     def __init__(self):
@@ -222,6 +231,7 @@ class StorageDevicePlugin(kvmagent.KvmAgent):
     NVME_DISCONNECT_PATH = "/storagedevice/nvme/disconnect"
     MULTIPATH_ENABLE_PATH = "/storagedevice/multipath/enable"
     MULTIPATH_DISABLE_PATH = "/storagedevice/multipath/disable"
+    GET_MULTIPATH_TOPOLOGY_PATH = "/storagedevice/multipath/topology"
     ATTACH_SCSI_LUN_PATH = "/storagedevice/scsilun/attach"
     DETACH_SCSI_LUN_PATH = "/storagedevice/scsilun/detach"
     DETACH_SCSI_DEV_PATH = "/storagedevice/scsilun/detachdev"
@@ -247,6 +257,7 @@ class StorageDevicePlugin(kvmagent.KvmAgent):
         http_server.register_async_uri(self.RAID_SMART_PATH, self.raid_smart)
         http_server.register_async_uri(self.RAID_LOCATE_PATH, self.raid_locate)
         http_server.register_async_uri(self.RAID_SELF_TEST_PATH, self.drive_self_test)
+        http_server.register_async_uri(self.GET_MULTIPATH_TOPOLOGY_PATH, self.get_multipath_topology)
 
     def stop(self):
         pass
@@ -1116,6 +1127,47 @@ class StorageDevicePlugin(kvmagent.KvmAgent):
     def disable_multipath(self, req):
         rsp = AgentRsp()
         lvm.disable_multipath()
+        return jsonobject.dumps(rsp)
+
+    @kvmagent.replyerror
+    def get_multipath_topology(self, req):
+        cmd = jsonobject.loads(req[http.REQUEST_BODY])
+        rsp = GetMultipathTopologyRsp()
+
+        def parse_multipath_topology():
+            # multipathd process needs to be running
+            r, multipaths = bash.bash_ro("multipathd show multipaths json")
+            if r != 0:
+                return {}
+            multipaths = jsonobject.loads(multipaths)
+            if not multipaths.maps:
+                return {}
+            results = {}
+            for mpath in multipaths.maps:
+                if not mpath.path_groups:
+                    continue
+
+                wwid = mpath.uuid
+                results.update({wwid: []})
+                for path_group in mpath.path_groups:
+                    results.get(wwid).append(Device("/dev/"+path_group.paths[0].dev, path_group.dm_st))
+            return results
+
+        def get_disk_by_wwid(wwid):
+            for cond in ['scsi-', "nvme-"]:
+                rp = os.path.realpath("/dev/disk/by-id/%s%s" % (cond, wwid))
+                if os.path.exists(rp):
+                    return rp
+
+        multipath_topology = parse_multipath_topology()
+        for wwid in cmd.wwids:
+            if wwid not in multipath_topology:
+                disk = get_disk_by_wwid(wwid)
+                if disk is not None:
+                    rsp.devices.update({wwid: [Device(disk, "active")]})
+            else:
+                rsp.devices.update({wwid: multipath_topology.get(wwid)})
+
         return jsonobject.dumps(rsp)
 
     @staticmethod
