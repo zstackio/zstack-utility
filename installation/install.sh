5ef2a77ddc02fb1bc0bf38255daf67c0c091c470
tao.yang <tao.yang@zstack.io>
2023-12-11 11:06:18 +0800

    <feature>[network]: improve zsv bond
    Resolves: ZSTAC-2737
    
    Change-Id: I637961636d7478636f777a7067646672756b6b62

--- installation/install.sh
+++ installation/install.sh
@@ -224,6 +224,7 @@ declare -a upgrade_params_array=(
     '4.6.0,-DupgradeLinuxGuestTools=true'
     '4.7.21,-DupgradeSecurityGroup=true'
     '4.7.21,-DupgradeSshKeyPairFromSystemTag=true'
+    '4.8.0,-DupgradeL2VirtualSwitchUplinkBonding=true'
 )
 #other than the upon params_array, this one could be persisted in zstack.properties
 declare -a upgrade_persist_params_array=(
