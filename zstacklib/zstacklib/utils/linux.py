164d8ebd84fdcda29b5d4b92e68650fd6fd3139b
qiuyu.zhang <qiuyu.zhang@zstack.io>
2023-12-12 20:11:36 +0800

    Revert "Merge branch '4.8.0' into '4.8.0'"
    This reverts merge request !4272

--- zstacklib/zstacklib/utils/linux.py
+++ zstacklib/zstacklib/utils/linux.py
@@ -2601,13 +2601,13 @@ def read_nic_carrier(path):
 
 def read_file_lines(path):
     if not os.path.exists(path):
-        return []
+        return None
     try:
         with open(path, 'r') as fd:
             return fd.readlines()
     except IOError as e:
         logger.error(e)
-        return []
+        return None
 
 
 def filter_file_lines_by_regex(path, regex):
