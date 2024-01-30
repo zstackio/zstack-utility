71eed4cba434ce190232f42dd7c8519d0a3f2972
J M <jin.ma@zstack.io>
2024-01-30 16:13:29 +0800

    <fix>[ha]: fix iscsi login and refresh
    setup startup in iscsi conf.
    
    Resolves: SUG-622
    
    Change-Id: I6d756a627a627078667461777461636d767a6e6d

--- zstacklib/zstacklib/utils/iscsi.py
+++ zstacklib/zstacklib/utils/iscsi.py
@@ -35,6 +35,7 @@ class IscsiLogin(object):
         device_path = os.path.join('/dev/disk/by-path/', 'ip-%s:%s-iscsi-%s-lun-%s' % (
             self.server_hostname, self.server_port, self.target, self.lun))
 
+        config_iscsi_startup_if_needed()
         shell.call('iscsiadm -m discovery -t sendtargets -p %s:%s' % (self.server_hostname, self.server_port))
 
         if self.chap_username and self.chap_password:
@@ -53,6 +54,8 @@ class IscsiLogin(object):
         s(False)
         if s.return_code != 0 and 'already present' not in s.stderr:
             s.raise_error()
+
+        shell.run("timeout 30 iscsiadm -m session -R")
 
         def wait_device_to_show(_):
             return bool(glob.glob(device_path))
@@ -88,3 +91,28 @@ class IscsiLogin(object):
         if not path:
             raise Exception('unable to find device path for disk id[%s]' % self.disk_id)
         return path
+
+
+def do_config_iscsi_startup():
+    conf_path = "/etc/iscsi/iscsid.conf"
+    if not os.path.exists(conf_path):
+        raise Exception(conf_path + " not found")
+
+    if linux.filter_file_lines_by_regex(conf_path, '^\s*iscsid.startup'):
+        return
+
+    with open(conf_path, 'a') as file:
+        file.write("\niscsid.startup = /bin/systemctl start iscsid.socket iscsiuio.socket\n")
+
+    return None
+
+
+config_iscsi_startup_needed = True
+
+
+@lock.lock('config_iscsi_startup_if_needed')
+def config_iscsi_startup_if_needed():
+    global config_iscsi_startup_needed
+    if config_iscsi_startup_needed:
+        do_config_iscsi_startup()
+        config_iscsi_startup_needed = False
