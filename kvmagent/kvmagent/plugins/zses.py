cc06b3e114d8e0b657b684f2bd3e85c9c19bf301
J M <jin.ma@zstack.io>
2024-03-01 17:56:00 +0800

    <fix>[nfsPrimaryStorage]: migrate check image dependency
    when nfs storage migrate, check image cache
    dependency when no image cache record on source nfs.
    
    Resolves: ZSTAC-60300
    
    Change-Id: I7369746b6563617175627770616b6c747a726d70
    
    
    (cherry picked from commit e1a19fd7dcb9f0e7ba10682b978d9a17cf673a10)

--- kvmagent/kvmagent/plugins/zses.py
+++ kvmagent/kvmagent/plugins/zses.py
@@ -61,6 +61,7 @@ class GetVolumeBaseImagePathRsp(AgentResponse):
     def __init__(self):
         super(GetVolumeBaseImagePathRsp, self).__init__()
         self.path = None
+        self.otherPaths = []
         self.size = None
 
 class GetQCOW2ReferenceRsp(AgentResponse):
@@ -165,12 +166,17 @@ class ZsesStoragePlugin(kvmagent.KvmAgent):
         if not os.path.basename(cmd.volumeInstallDir).endswith(cmd.volumeUuid):
             raise Exception('maybe you pass a wrong install dir')
 
-        path = linux.get_qcow2_base_image_recusively(cmd.volumeInstallDir, cmd.imageCacheDir)
-        if not path:
-            return jsonobject.dumps(rsp)
+        paths = linux.get_qcow2_base_images_recusively(cmd.volumeInstallDir, cmd.imageCacheDir)
+        current_chain = linux.qcow2_get_file_chain(cmd.volumeInstallPath)
 
-        rsp.path = path
-        rsp.size = linux.get_qcow2_file_chain_size(path)
+        for path in current_chain:
+            if path in paths:
+                rsp.path = path
+                rsp.size = linux.get_qcow2_file_chain_size(rsp.path)
+                paths.remove(path)
+                break
+
+        rsp.otherPaths = paths
         return jsonobject.dumps(rsp)
 
     @kvmagent.replyerror
