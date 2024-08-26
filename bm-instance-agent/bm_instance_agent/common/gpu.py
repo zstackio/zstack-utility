4ef63d1d7246f63bef5da6ab05ae57bbc6100ab2
shenjin <jin.shen@zstack.io>
2024-08-26 14:40:52 +0800

    <fix>[bm-instance-agent]: add isDriverLoaded param for baremetal2 gpu
    add isDriverLoaded param for baremetal2 chassis
    gpu device, and add collect gpu info for tianshu,
    huawei
    
    Resolves: ZSTAC-66833
    
    Change-Id: I6a6472707275777971736d747a71627978626e71

---
+++ bm-instance-agent/bm_instance_agent/common/gpu.py
@@ -0,0 +1,152 @@
+import json
+
+from oslo_log import log as logging
+
+LOG = logging.getLogger(__name__)
+
+
+def parse_nvidia_gpu_output(output):
+    gpuinfos = []
+    for part in output.split('\n'):
+        if len(part.strip()) == 0:
+            continue
+        infos = part.split(',')
+        gpuinfo = {}
+        pci_device_address = infos[0].strip().lower()
+        if len(pci_device_address.split(':')[0]) == 8:
+            pci_device_address = pci_device_address[4:].lower()
+        gpuinfo["pciAddress"] = pci_device_address
+        gpuinfo["memory"] = infos[1].strip()
+        gpuinfo["power"] = infos[2].strip()
+        gpuinfo["serialNumber"] = infos[3].strip()
+        gpuinfos.append(gpuinfo)
+    return gpuinfos
+
+
+def parse_amd_gpu_output(output):
+    gpuinfos = []
+    try:
+        gpu_info_json = json.loads(output.strip())
+        for card_name, card_data in gpu_info_json.items():
+            gpuinfo = {}
+            pci_device_address = card_data['PCI Bus'].lower()
+            if len(pci_device_address.split(':')[0]) == 8:
+                pci_device_address = pci_device_address[4:].lower()
+
+            gpuinfo["pciAddress"] = pci_device_address
+            gpuinfo["memory"] = card_data['VRAM Total Memory (B)']
+            gpuinfo["power"] = card_data['Average Graphics Package Power (W)']
+            gpuinfo["serialNumber"] = card_data['Serial Number']
+            gpuinfos.append(gpuinfo)
+    except Exception as e:
+        LOG.error("amd query gpu is error, %s " % e)
+
+    return gpuinfos
+
+
+def parse_hy_gpu_output(output):
+    gpuinfos = []
+    try:
+        gpu_info_json = json.loads(output)
+        for card_name, card_data in gpu_info_json.items():
+            gpuinfo = {}
+            pci_device_address = card_data['PCI Bus'].lower()
+            if len(pci_device_address.split(':')[0]) == 8:
+                pci_device_address = pci_device_address[4:].lower()
+
+            gpuinfo["pciAddress"] = pci_device_address
+            gpuinfo["memory"] = card_data['Available memory size (MiB)'] + " MiB"
+            gpuinfo["power"] = card_data['Max Graphics Package Power (W)']
+            gpuinfo["serialNumber"] = card_data['Serial Number']
+            gpuinfos.append(gpuinfo)
+    except Exception as e:
+        LOG.error("haiguang query gpu is error, %s " % e)
+
+    return gpuinfos
+
+
+def get_huawei_npu_id(npu_id_output):
+    for line in npu_id_output.splitlines():
+        line = line.strip()
+        if not line:
+            continue
+        if "NPU ID" in line:
+            return line.split(":")[1].strip()
+    return None
+
+
+def parse_huawei_gpu_output_by_npu_id(output):
+    gpuinfos = []
+    gpuinfo = {}
+    for line in output.splitlines():
+        line = line.strip()
+        if not line:
+            continue
+        if "Serial Number" in line:
+            gpuinfo["serialNumber"] = line.split(":")[1].strip()
+        elif "PCIe Bus Info" in line:
+            gpuinfo["pciAddress"] = line.partition(": ")[-1].strip().lower()
+        elif "DDR Capacity(MB)" in line:
+            gpuinfo["memory"] = line.split(":")[1].strip() + " MB"
+
+    gpuinfos.append(gpuinfo)
+    return gpuinfos
+
+
+def parse_tianshu_gpu_output(output):
+    gpuinfos = []
+    for part in output.split('\n'):
+        if len(part.strip()) == 0:
+            continue
+        infos = part.split(',')
+        gpuinfo = {}
+        pci_device_address = infos[0].strip()
+        if len(pci_device_address.split(':')[0]) == 8:
+            pci_device_address = pci_device_address[4:].lower()
+
+        gpuinfo["pciAddress"] = pci_device_address
+        gpuinfo["memory"] = infos[1].strip()
+        gpuinfo["power"] = infos[2].strip()
+        gpuinfo["serialNumber"] = infos[3].strip()
+        gpuinfos.append(gpuinfo)
+
+    return gpuinfos
+
+
+def get_nvidia_gpu_basic_info_cmd(iswindows=False):
+    cmd = "nvidia-smi --query-gpu=gpu_bus_id,memory.total,power.limit,gpu_serial --format=csv,noheader"
+    if iswindows:
+        cmd = cmd.replace(" ", "|")
+    return cmd
+
+
+def get_amd_gpu_basic_info_cmd(iswindows=False):
+    cmd = "rocm-smi --showbus --showmeminfo vram --showpower --showserial --json"
+    if iswindows:
+        cmd = cmd.replace(" ", "|")
+    return cmd
+
+
+def get_hy_gpu_basic_info_cmd(iswindows=False):
+    cmd = "hy-smi --showserial --showmaxpower --showmemavailable --showbus --json"
+    if iswindows:
+        cmd = cmd.replace(" ", "|")
+    return cmd
+
+
+def get_tianshu_gpu_basic_info_cmd(iswindows=False):
+    cmd = "ixsmi --query-gpu=gpu_bus_id,memory.total,gpu.power.limit,gpu_serial --format=csv,noheader"
+    if iswindows:
+        cmd = cmd.replace(" ", "|")
+    return cmd
+
+
+def get_huawei_gpu_npu_id_cmd():
+    return "npu-smi info -l"
+
+
+def get_huawei_gpu_basic_info_cmd(npu_id, iswindows=False):
+    cmd = "npu-smi info -t board -i %s;npu-smi info -i %s -t memory" % (npu_id, npu_id)
+    if iswindows:
+        cmd = cmd.replace(" ", "|")
+    return cmd
