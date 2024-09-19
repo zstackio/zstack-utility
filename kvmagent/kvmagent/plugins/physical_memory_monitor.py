8a05f548616c1664c3323385a0a35d5fad407bac
xinhao.huang <xinhao.huang@zstack.io>
2024-09-19 10:18:10 +0800

    <fix>[physical_memory_monitor]: fix ecc error wrong alarm
    Resolves: ZHCI-2508
    
    Change-Id: I6e61706a77696e757779776c6f736a6a67707774

--- kvmagent/kvmagent/plugins/physical_memory_monitor.py
+++ kvmagent/kvmagent/plugins/physical_memory_monitor.py
@@ -63,16 +63,18 @@ class PhysicalMemoryMonitor(kvmagent.KvmAgent):
 			zero error return: "edac-util: No errors to report."
 			non-zero error return: "csrow0: ch0: 43722040 Corrected Errors"
 		'''
-		r, o, e = bash.bash_roe("edac-util --report=default")
+		r, o, e = bash.bash_roe("edac-util --report=default", ret_code=-1)
 		# ZHCI-1484: The edac module will not be loaded in some env and command returns 'No memory controller data found',
 		# add a judgment to work around it.
 		# ZHCI-1502: 'No memory controller data found' may be output in std_out or std_error.
-		if r == 0 and not self.trigger_flag and ("No errors to report" not in o and "No memory controller data found" not in (o + (e if e else ""))):
+		if "No memory controller data found" in (o + (e if e else "")):
+			self.trigger_flag = False
+		elif "No errors to report" in (o + (e if e else "")):
+			self.trigger_flag = False
+		elif not self.trigger_flag:
 			self.send_physical_memory_ecc_error_alarm_to_mn(o)
 			self.trigger_flag = True
-		else:
-			self.trigger_flag = False
-			
+
 		thread.timer(self.interval, self.monitor_physical_memory_ecc_error, args=[time_lock_now]).start()
 	
 	def send_physical_memory_ecc_error_alarm_to_mn(self, detail):
